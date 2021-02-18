44cf39da3583c41cfb921ecf1dbe52f7cdb8f643
Daniel Lemire <dlemire@rcs-power9-talos>
2021-02-17 19:43:46 -0500

    Tweaking Power

--- include/simdutf/ppc64/implementation.h
+++ include/simdutf/ppc64/implementation.h
@@ -17,11 +17,11 @@ public:
       : simdutf::implementation("ppc64", "PPC64 ALTIVEC",
                                  internal::instruction_set::ALTIVEC) {}
   simdutf_warn_unused bool validate_utf8(const char *buf, size_t len) const noexcept final;
-  simdutf_warn_unused bool validate_utf16(const char *buf, size_t len) const noexcept final;
-  simdutf_warn_unused size_t convert_utf8_to_utf16(const char * buf, size_t len, char* utf16_output) const noexcept final;
-  simdutf_warn_unused size_t convert_valid_utf8_to_utf16(const char * buf, size_t len, char* utf16_buffer) const noexcept final;
-  simdutf_warn_unused size_t convert_utf16_to_utf8(const char * buf, size_t len, char* utf8_buffer) const noexcept final;
-  simdutf_warn_unused size_t convert_valid_utf16_to_utf8(const char * buf, size_t len, char* utf8_buffer) const noexcept final;
+  simdutf_warn_unused bool validate_utf16(const char16_t *buf, size_t len) const noexcept final;
+  simdutf_warn_unused size_t convert_utf8_to_utf16(const char * buf, size_t len, char16_t* utf16_output) const noexcept final;
+  simdutf_warn_unused size_t convert_valid_utf8_to_utf16(const char * buf, size_t len, char16_t* utf16_buffer) const noexcept final;
+  simdutf_warn_unused size_t convert_utf16_to_utf8(const char16_t * buf, size_t len, char* utf8_buffer) const noexcept final;
+  simdutf_warn_unused size_t convert_valid_utf16_to_utf8(const char16_t * buf, size_t len, char* utf8_buffer) const noexcept final;
 };
 
 } // namespace ppc64
