a30ad8ba96601c0c910ef816bee7c8dc0e8db880
Daniel Lemire <lemire@gmail.com>
2021-03-01 18:26:57 -0500

    Patching UTF16=>UTF8 conversions in arm64 (using scalar).

--- src/arm64/implementation.cpp
+++ src/arm64/implementation.cpp
@@ -206,12 +206,12 @@ simdutf_warn_unused size_t implementation::convert_valid_utf8_to_utf16(const cha
   return utf16_output - start;
 }
 
-simdutf_warn_unused size_t implementation::convert_utf16_to_utf8(const char16_t* /*buf*/, size_t /*len*/, char* /*utf8_output*/) const noexcept {
-  return 0; // stub
+simdutf_warn_unused size_t implementation::convert_utf16_to_utf8(const char16_t* buf, size_t len, char* utf8_output) const noexcept {
+  return utf16_to_utf8::scalar_convert(buf, len, utf8_output);
 }
 
-simdutf_warn_unused size_t implementation::convert_valid_utf16_to_utf8(const char16_t* /*buf*/, size_t /*len*/, char* /*utf8_output*/) const noexcept {
-  return 0; // stub
+simdutf_warn_unused size_t implementation::convert_valid_utf16_to_utf8(const char16_t* buf, size_t len, char* utf8_output) const noexcept {
+  return utf16_to_utf8::scalar_convert(buf, len, utf8_output);
 }
 
 } // namespace SIMDUTF_IMPLEMENTATION
