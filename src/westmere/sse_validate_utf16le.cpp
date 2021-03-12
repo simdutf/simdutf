860a131f87115b1481dd1b856e8ffc8a37340f71
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2021-03-12 18:52:19 +0100

    Check 16 words per iteration in validate_utf16

--- src/westmere/sse_validate_utf16le.cpp
+++ src/westmere/sse_validate_utf16le.cpp
@@ -46,55 +46,64 @@
 const char16_t* sse_validate_utf16le(const char16_t* input, size_t size) {
     const char16_t* end = input + size;
 
-    const __m128i v_d800 = _mm_set1_epi32(0xd800'd800);
-    const __m128i v_f800 = _mm_set1_epi32(0xf800'f800);
-    const __m128i v_fc00 = _mm_set1_epi32(0xfc00'fc00);
-    const __m128i v_dc00 = _mm_set1_epi32(0xdc00'dc00);
+    const __m128i v_d8 = _mm_set1_epi8(int8_t(0xd8));
+    const __m128i v_f8 = _mm_set1_epi8(int8_t(0xf8));
+    const __m128i v_fc = _mm_set1_epi8(int8_t(0xfc));
+    const __m128i v_dc = _mm_set1_epi8(int8_t(0xdc));
 
-    while (input + 8 < end) {
-        const __m128i in = _mm_loadu_si128((__m128i*)input);
+    while (input + 16 < end) {
+        // 0. Load data: since the validation takes into account only higher
+        //    byte of each byte, we compress the two vectors into one which
+        //    consists only the higher bytes.
+        const __m128i in0 = _mm_loadu_si128((__m128i*)(input + 0*8));
+        const __m128i in1 = _mm_loadu_si128((__m128i*)(input + 1*8));
+
+        const __m128i t0  = _mm_srli_epi16(in0, 8);
+        const __m128i t1  = _mm_srli_epi16(in1, 8);
+
+        const __m128i in  = _mm_packus_epi16(t0, t1);
 
         // 1. Check whether we have any 0xD800..DFFF word (0b1101'1xxx'yyyy'yyyy).
-        const __m128i surrogates_wordmask = _mm_cmpeq_epi16(_mm_and_si128(in, v_f800), v_d800);
+        const __m128i surrogates_wordmask = _mm_cmpeq_epi8(_mm_and_si128(in, v_f8), v_d8);
         const uint16_t surrogates_bitmask = _mm_movemask_epi8(surrogates_wordmask);
         if (surrogates_bitmask == 0x0000) {
-            input += 8;
+            input += 16;
         } else {
             // 2. We have some surrogates that have to be distinguished:
             //    - low  surrogates: 0b1101'10xx'yyyy'yyyy (0xD800..0xDBFF)
             //    - high surrogates: 0b1101'11xx'yyyy'yyyy (0xDC00..0xDFFF)
             //
-            //    Fact: high surrogate has 11th bit set
+            //    Fact: high surrogate has 11th bit set (3rd bit in the higher word)
 
             // V - non-surrogate words
             //     V = not surrogates_wordmask
             const uint16_t V = ~surrogates_bitmask;
 
             // H - word-mask for high surrogates: the six highest bits are 0b1101'11
-            const __m128i vH = _mm_cmpeq_epi16(_mm_and_si128(in, v_fc00), v_dc00);
+            const __m128i vH = _mm_cmpeq_epi8(_mm_and_si128(in, v_fc), v_dc);
             const uint16_t H = _mm_movemask_epi8(vH);
 
             // L - word mask for low surrogates
             //     L = not H and surrogates_wordmask
             const uint16_t L = ~H & surrogates_bitmask;
 
-            const uint16_t a = L & (H >> 2);  // A low surrogate must be followed by high one.
+            const uint16_t a = L & (H >> 1);  // A low surrogate must be followed by high one.
                                               // (A low surrogate placed in the 7th register's word
                                               // is an exception we handle.)
-            const uint16_t b = a << 2;        // Just mark that the opposite fact is hold,
+            const uint16_t b = a << 1;        // Just mark that the opposite fact is hold,
                                               // thanks to that we have only two masks for valid case.
             const uint16_t c = V | a | b;     // Combine all the masks into the final one.
 
             if (c == 0xffff)
                 // The whole input register contains valid UTF16, i.e.,
                 // either single words or proper surrogates.
-                input += 8;
-            else if (c == 0x3fff)
+                input += 16;
+            else if (c == 0x7fff)
                 // The 7 lower words of the input register contains valid UTF16.
-                // The 7th word may be either a low or high surrogate. It the next
+                // The 15th word may be either a low or high surrogate. It the next
                 // iteration we 1) check if the low surrogate is followed by a high
                 // one, 2) reject sole hight surrogate.
-                input += 7;
+                input += 15;
             else
                 return nullptr;
         }
