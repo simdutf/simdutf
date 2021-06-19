5cca10b4b53b6f3d079e8f21b91d85432ca8b934
Daniel Lemire <lemire@gmail.com>
2021-06-19 13:42:46 -0400

    Hacking the port.

--- src/arm64/arm_convert_utf16_to_utf8.cpp
+++ src/arm64/arm_convert_utf16_to_utf8.cpp
@@ -42,105 +42,229 @@
     and the number of output bytes in the compressed vector register.
     Each entry occupies 17 bytes.
 
-    Ad 3.
-
-    The input is normalized into 32-bit words, it spans two registers.
-    Next the 32-bit words are converted into UCS4. For non-surrogate
-    entries there's no need to do anything. In the case of words
-    containing surrogate pairs, the RFC-imposed algorithm is used
-    (RFC-2781, 2.2 Decoding UTF-16).
-
-    Next, the UCS32 is converted into UTF8 withing 32-bit words. Finally
-    the registers are compressed. Then we complete missing bits in UTF8
-    bytes.
-
-    We need 256-entry lookup table to perform expansion into UCS4. Each
-    entry contains two shuffle patterns (32 bytes). We need another
-    256-entry lookup table to get a compression pattern, UTF8 bit patterns,
-    and the number of output bytes in the compressed vector register.
-    Each entry occupies 33 bytes.
 
     Summarize:
-    - We need four 256-entry tables that have 25344 bytes in total.
+    - We need two 256-entry tables that have 8704 bytes in total.
 */
 
-int convert_UCS4_to_UTF8(uint32x4_t in, uint8x16_t& out) {
-  const uint32x4_t v_80   = vmovq_n_u32(0x00000080);
-  const uint32x4_t v_7f   = vmovq_n_u32(0x0000007f);
-  const uint32x4_t v_7ff  = vmovq_n_u32(0x000007ff);
-  const uint32x4_t v_ffff = vmovq_n_u32(0x0000ffff);
+/*
+  Returns a pair: the first unprocessed byte from buf and utf8_output
+  A scalar routing should carry on the conversion of the tail.
+*/
+std::pair<const char16_t*, char*> sse_convert_utf16_to_utf8(const char16_t* buf, size_t len, char* utf8_output) {
 
-  const uint32x4_t mask_1_byte      = vcgtq_u32 (in, v_80);
-  const uint32x4_t mask_2_3_4_bytes = vcgtq_u32(in, v_7f);
-  const uint32x4_t mask_3_4_bytes   = vcgtq_u32(in, v_7ff);
-  const uint32x4_t mask_4_bytes     = vcgtq_u32(in, v_ffff);
 
-  // 0a. convert surrogate pairs into UCS
-  {
-      const uint32x4_t firstpart = vandq_u32(in, vmovq_n_u32(0x03ff0000));
-      const uint32x4_t secondpart = vandq_u32(in, vmovq_n_u32(0x000003ff));
-      const uint32x4_t shiftedfirstpart =  vshrq_n_u32(firstpart, 10);
-      const uint32x4_t t0 = vorrq_u32(shiftedfirstpart, secondpart); // join 10-bit words into single 20-bit word
-      const uint32x4_t t1 = vaddq_u32 (t0, vmovq_n_u32(0x00010000)); // add 0x1'0000
-      in = vbslq_u32(in, t1, mask_4_bytes);
-  }
+  const char16_t* end = buf + len;
 
-  // 1a. shift 6-bit words - step #1
-  // 1. [00000000|00000000|00000000|0xxxxxxx]
-  // 2. [00000000|00000000|00000yyy|yyxxxxxx]
-  // 3. [00000000|00000000|zzzzyyyy|yyxxxxxx]
-  // 4. [00000000|0000wwzz|zzzzyyyy|yyxxxxxx]
+  const uint16x8_t v_0000 = vmovq_n_u16(0);
+  const uint16x8_t v_f800 = vmovq_n_u16((uint16_t)0xf800);
+  const uint16x8_t v_d800 = vmovq_n_u16((uint16_t)0xd800);
+  const uint16x8_t v_c080 = vmovq_n_u16((uint16_t)0xc080);
 
-  const uint32x4_t v_0000003f = vmovq_n_u32(0x0000003f);
-  const uint32x4_t v_00003f00 = vmovq_n_u32(0x00003f00);
-  const uint32x4_t v_003f0000 = vmovq_n_u32(0x003f0000);
-  const uint32x4_t v_0f000000 = vmovq_n_u32(0x0f000000);
-  uint32x4_t byte1 = vandq_u32(in, v_0000003f);
-  uint32x4_t byte2 = vshlq_u32(in, 2);
-  byte2 = vandq_u32(byte2, v_00003f00); // byte2 = [00000000|00000000|00yyyyyy|00000000]
-  uint32x4_t byte3 = vshlq_u32(in, 4);
-  byte3 = vandq_u32(byte3, v_003f0000); // byte3 = [00000000|00zzzzzz|00000000|00000000]
-  uint32x4_t byte4 = vshlq_u32(in, 6);
-  byte4 = vandq_u32(byte4, v_0f000000); // byte4 = [000000ww|00000000|00000000|00000000]
+  while (buf + 16 <= end) {
 
-  // merged = [000000ww|00zzzzzz|00yyyyyy|00xxxxxx] for 2, 3, 4 UTF-8 bytes
-  uint32x4_t merged;
-  merged = vorrq_u32(vorrq_u32(byte1, byte2),
-                        vorrq_u32(byte3, byte4));
-  // merged = [00000000|00000000|00000000|0xxxxxxx] for 1 UTF-8 byte
+    uint16x8_t in = vld1q_u16(buf);
 
-  merged = vbslq_u32(mask_1_byte, in, mask_1_byte);
- 
-  // Count how many UTF-8 bytes produce each 32-bit word. We are adding three bitmask,
-  // thus the addition result will be in range 0..3 (it spans 2 bits), where:
-  // * 0 = 0b00 - 1 byte
-  // * 1 = 0b01 - 2 bytes
-  // * 2 = 0b10 - 3 bytes
-  // * 3 = 0b11 - 4 bytes
-  // We keep just bits #7 and #14 of the masks. After addition the bit #7 will
-  // hold the lower bit of the sum, bit #15 -- the higher bit.
-  const uint32x4_t v_00004080      = vmovq_n_u32(0x00004080);
-  const uint32x4_t cnt_2_3_4_bytes = vandq_u32(mask_2_3_4_bytes, v_00004080);
-  const uint32x4_t cnt_3_4_bytes   = vandq_u32(mask_3_4_bytes, v_00004080);
-  const uint32x4_t cnt_4_bytes     = vandq_u32(mask_4_bytes, v_00004080);
-  const uint32x4_t count = vaddq_u32(vaddq_u32(cnt_2_3_4_bytes, cnt_3_4_bytes), cnt_4_bytes);
+    if(vmaxvq_u16(in) <= 0x7F) { // ASCII fast path!!!!
+        // It is common enough that we have sequences of 16 consecutive ASCII characters.
+        uint16x8_t nextin = vld1q_u16(buf);
+        if(vmaxvq_u16(nextin) > 0x7F) {
+          // 1. pack the bytes
+          // obviously suboptimal.
+          uint8x8_t utf8_packed = vmovn_u16(in);
+          // 2. store (8 bytes)
+          vst1_u8(utf8_output, utf8_packed);
+          // 3. adjust pointers
+          buf += 8;
+          utf8_output += 8;
+          in = nextin;
+        } else {
+          // 1. pack the bytes
+          // obviously suboptimal.
+          uint8x16_t utf8_packed = vmovn_high_u16(vmovn_u16(in), nextin);
+          // 2. store (16 bytes)
+          vst1q_u8(utf8_output, utf8_packed);
+          // 3. adjust pointers
+          buf += 16;
+          utf8_output += 16;
+          continue; // we are done for this round!
+        }
+    }
 
-  // Now, when we gather MSB, we obtain a bitmask
-  // like this: 00hg00fe00dc00ba, where hg, fe, dc and ba are the two-bit counters.
-  // Compress a sparse 16-bit word bitmask into 8-bit one: hgdcfeba
-  const uint16x8_t bitmask = { 0x0201 , 0x0000, 0x2010, 0x0000, 0x0804, 0x0000, 0x8040, 0x0000 };
-  uint16x8_t m0 = vandq_u16(input, bitmask);
-  const uint8_t mask = vaddvq_u8(minput);
+    if (vmaxvq_u16(in) <= 0x7FF) {
+          // 1. prepare 2-byte values
+          // input 16-bit word : [0000|0aaa|aabb|bbbb] x 8
+          // expected output   : [110a|aaaa|10bb|bbbb] x 8
+          const uint16x8_t v_1f00 = vmovq_n_u16((int16_t)0x1f00);
+          const uint16x8_t v_003f = vmovq_n_u16((int16_t)0x003f);
 
-  const auto& to_utf8 = tables::utf16_to_utf8::ucs4_to_utf8[mask];
-  // merged = [000000ww|00zzzzzz|00yyyyyy|00xxxxxx]
-  //          [00000000|00000000|00000000|0xxxxxxx]
-  merged = vorrq_u32(merged, vld1q_u8(to_utf8.const_bits_mask));
-  out = vqtbl2q_u8(merged, vld1q_u8(to_utf8.shuffle));
+          // t0 = [000a|aaaa|bbbb|bb00]
+          const uint16x8_t t0 = vshlq_u16(in, 2);
+          // t1 = [000a|aaaa|0000|0000]
+          const uint16x8_t t1 = vandq_u16(t0, v_1f00);
+          // t2 = [0000|0000|00bb|bbbb]
+          const uint16x8_t t2 = vandq_u16(in, v_003f);
+          // t3 = [000a|aaaa|00bb|bbbb]
+          const uint16x8_t t3 = vorrq_u16(t1, t2);
+          // t4 = [110a|aaaa|10bb|bbbb]
+          const uint16x8_t t4 = vorrq_u16(t3, v_c080);
 
-  return to_utf8.output_bytes;
+          // 2. merge ASCII and 2-byte codewords
+          const uint16x8_t v_007f = vmovq_n_u16((uint16_t)0x007F);
+          const uint16x8_t one_byte_bytemask = vcleq_u16(in, v_007f);
+          const uint8x16_t utf8_unpacked = vreinterpretq_u8_u16(vbslq_u16(t4, in, one_byte_bytemask));
+
+          // 3. prepare bitmask for 8-bit lookup
+
+
+          const uint16x8_t mask = { 0x0001, 0x0010, 
+                                    0x0002, 0x0020, 
+                                    0x0004, 0x0040, 
+                                    0x0008, 0x0080};
+          uint16_t m2 = vaddvq_u16(vandq_u16(one_byte_bytemask, mask));
+          // 4. pack the bytes
+          const uint8_t* row = &tables::utf16_to_utf8::pack_1_2_utf8_bytes[m2][0];
+          const uint8x16_t shuffle = vld1q_u8(row + 1);
+          const uint8x16_t utf8_packed = vqtbl1q_u8(utf8_unpacked, shuffle);
+
+          // 5. store bytes
+          vst1q_u8(utf8_output, utf8_packed);
+
+          // 6. adjust pointers
+          buf += 8;
+          utf8_output += row[0];
+          continue;
+
+    }
+
+    // no bits set above 7th bit
+    const __m128i one_byte_bytemask = _mm_cmpeq_epi16(_mm_and_si128(in, v_ff80), v_0000);
+    const uint16_t one_byte_bitmask = static_cast<uint16_t>(_mm_movemask_epi8(one_byte_bytemask));
+
+    // no bits set above 11th bit
+    const __m128i one_or_two_bytes_bytemask = _mm_cmpeq_epi16(_mm_and_si128(in, v_f800), v_0000);
+    const uint16_t one_or_two_bytes_bitmask = static_cast<uint16_t>(_mm_movemask_epi8(one_or_two_bytes_bytemask));
+
+    // 1. Check if there are any surrogate word in the input chunk.
+    //    We have also deal with situation when there is a suggogate word
+    //    at the end of a chunk.
+    const __m128i surrogates_bytemask = _mm_cmpeq_epi16(_mm_and_si128(in, v_f800), v_d800);
+
+    // bitmask = 0x0000 if there are no surrogates
+    //         = 0xc000 if the last word is a surrogate
+    const uint16_t surrogates_bitmask = static_cast<uint16_t>(_mm_movemask_epi8(surrogates_bytemask));
+    // It might seem like checking for surrogates_bitmask == 0xc000 could help. However,
+    // it is likely an uncommon occurrence.
+    if (surrogates_bitmask == 0x0000) {
+      // case: words from register produce either 1, 2 or 3 UTF-8 bytes
+        const __m128i dup_even = _mm_setr_epi16(0x0000, 0x0202, 0x0404, 0x0606,
+                                                0x0808, 0x0a0a, 0x0c0c, 0x0e0e);
+
+        /* In this branch we handle three cases:
+           1. [0000|0000|0ccc|cccc] => [0ccc|cccc]                           - single UFT-8 byte
+           2. [0000|0bbb|bbcc|cccc] => [110b|bbbb], [10cc|cccc]              - two UTF-8 bytes
+           3. [aaaa|bbbb|bbcc|cccc] => [1110|aaaa], [10bb|bbbb], [10cc|cccc] - three UTF-8 bytes
+
+          We expand the input word (16-bit) into two words (32-bit), thus
+          we have room for four bytes. However, we need five distinct bit
+          layouts. Note that the last byte in cases #2 and #3 is the same.
+
+          We precompute byte 1 for case #1 and the common byte for cases #2 & #3
+          in register t2.
+
+          We precomputer byte 1 for case #3 and -- **conditionally** -- precompute
+          either byte 1 for case #2 or byte 2 for case #3. Note that they
+          differ by exactly one bit.
+
+          Finally from these two words we build proper UTF-8 sequence, taking
+          into account the case (i.e, the number of bytes to write).
+        */
+#define vec(x) _mm_set1_epi16(static_cast<uint16_t>(x))
+        const __m128i t0 = _mm_shuffle_epi8(in, dup_even);
+        const __m128i t1 = _mm_and_si128(t0, vec(0b0011'1111'0111'1111));
+        const __m128i t2 = _mm_or_si128 (t1, vec(0b1000'0000'0000'0000));
+
+
+        const __m128i s0 = _mm_srli_epi16(in, 4);
+        const __m128i s1 = _mm_and_si128(s0, vec(0b0000'1111'1111'1100));
+        const __m128i s2 = _mm_maddubs_epi16(s1, vec(0x0140));
+        const __m128i s3 = _mm_or_si128(s2, vec(0b1100'0000'1110'0000));
+        const __m128i m0 = _mm_andnot_si128(one_or_two_bytes_bytemask, vec(0b0100'0000'0000'0000));
+        const __m128i s4 = _mm_xor_si128(s3, m0);
+#undef vec
+
+        // 4. expand words 16-bit => 32-bit
+        const __m128i out0 = _mm_unpacklo_epi16(t2, s4);
+        const __m128i out1 = _mm_unpackhi_epi16(t2, s4);
+
+        // 5. compress 32-bit words into 1, 2 or 3 bytes -- 2 x shuffle
+        const uint16_t mask = (one_byte_bitmask & 0x5555) |
+                              (one_or_two_bytes_bitmask & 0xaaaa);
+        if(mask == 0) {
+          // We only have three-byte words. Use fast path.
+          const __m128i shuffle = _mm_setr_epi8(2,3,1,6,7,5,10,11,9,14,15,13,-1,-1,-1,-1);
+          const __m128i utf8_0 = _mm_shuffle_epi8(out0, shuffle);
+          const __m128i utf8_1 = _mm_shuffle_epi8(out1, shuffle);
+          _mm_storeu_si128((__m128i*)utf8_output, utf8_0);
+          utf8_output += 12;
+          _mm_storeu_si128((__m128i*)utf8_output, utf8_1);
+          utf8_output += 12;
+          buf += 8;
+          continue;
+        }
+        const uint8_t mask0 = uint8_t(mask);
+
+        const uint8_t* row0 = &tables::utf16_to_utf8::pack_1_2_3_utf8_bytes[mask0][0];
+        const __m128i shuffle0 = _mm_loadu_si128((__m128i*)(row0 + 1));
+        const __m128i utf8_0 = _mm_shuffle_epi8(out0, shuffle0);
+
+        const uint8_t mask1 = static_cast<uint8_t>(mask >> 8);
+
+        const uint8_t* row1 = &tables::utf16_to_utf8::pack_1_2_3_utf8_bytes[mask1][0];
+        const __m128i shuffle1 = _mm_loadu_si128((__m128i*)(row1 + 1));
+        const __m128i utf8_1 = _mm_shuffle_epi8(out1, shuffle1);
+
+        _mm_storeu_si128((__m128i*)utf8_output, utf8_0);
+        utf8_output += row0[0];
+        _mm_storeu_si128((__m128i*)utf8_output, utf8_1);
+        utf8_output += row1[0];
+
+        buf += 8;
+    // surrogate pair(s) in a register
+    } else {
+      // Let us do a scalar fallback.
+      int k = 0;
+      for(; k < 15; k++) {
+        uint16_t word = buf[k];
+        if((word & 0xFF80)==0) {
+          *utf8_output++ = char(word);
+        } else if((word & 0xF800)==0) {
+          *utf8_output++ = char((word>>6) | 0b11000000);
+          *utf8_output++ = char((word & 0b111111) | 0b10000000);
+        } else if((word &0xF800 ) != 0xD800) {
+          *utf8_output++ = char((word>>12) | 0b11100000);
+          *utf8_output++ = char(((word>>6) & 0b111111) | 0b10000000);
+          *utf8_output++ = char((word & 0b111111) | 0b10000000);
+        } else {
+          // must be a surrogate pair
+          uint16_t diff = uint16_t(word - 0xD800);
+          uint16_t next_word = buf[k+1];
+          k++;
+          uint16_t diff2 = uint16_t(next_word - 0xDC00);
+          if((diff | diff2) > 0x3FF)  { return std::make_pair(nullptr, utf8_output); }
+          uint32_t value = (diff << 10) + diff2 + 0x10000;
+          *utf8_output++ = char((value>>18) | 0b11110000);
+          *utf8_output++ = char(((value>>12) & 0b111111) | 0b10000000);
+          *utf8_output++ = char(((value>>6) & 0b111111) | 0b10000000);
+          *utf8_output++ = char((value & 0b111111) | 0b10000000);
+        }
+      }
+      buf += k;
+    }
+  } // while
+
+  return std::make_pair(buf, utf8_output);
 }
-
 
 
 /*
