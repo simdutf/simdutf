522216b27f076f08bc27815bafea4aef11b4e6e8
Daniel Lemire <dlemire@rcs-power9-talos>
2021-08-09 17:19:52 -0400

    Minor fixes.

--- src/ppc64/implementation.cpp
+++ src/ppc64/implementation.cpp
@@ -27,11 +27,12 @@ simdutf_unused simdutf_really_inline simd8<bool> must_be_continuation(const simd
 
 simdutf_really_inline simd8<bool> must_be_2_3_continuation(const simd8<uint8_t> prev2, const simd8<uint8_t> prev3) {
   simd8<uint8_t> is_third_byte  = prev2.saturating_sub(0b11100000u-1); // Only 111_____ will be > 0
-  simd8<uint8_t> is_fourth_byte = prev3.saturating_sub(0b11110000u-1); // Only 1111____ will be > 0
+  simd8<uint8_t> is_fourth_byte = prev3.saturating_sub(0b11110000u-1); // Only 1111____ will be > 0#include "haswell/avx2_convert_utf8_to_utf16.cpp"
   // Caller requires a bool (all 1's). All values resulting from the subtraction will be <= 64, so signed comparison is fine.
   return simd8<int8_t>(is_third_byte | is_fourth_byte) > int8_t(0);
 }
 
+#include "ppc64/ppc64_convert_utf8_to_utf16.cpp"
 } // unnamed namespace
 } // namespace SIMDUTF_IMPLEMENTATION
 } // namespace simdutf
--- src/ppc64/ppc64_convert_utf8_to_utf16.cpp
+++ src/ppc64/ppc64_convert_utf8_to_utf16.cpp
@@ -10,14 +10,13 @@ size_t convert_masked_utf8_to_utf16(const char *input,
                            uint64_t utf8_end_of_code_point_mask,
                            char16_t *&utf16_output) {
   // We first try a few fast paths.
-  const vecu8 in = (vecu8)(vec_vsx_ld(0, reinterpret_cast<const uint8_t *>(values)));
+  const vecu8 in = (vecu8)(vec_vsx_ld(0, reinterpret_cast<const uint8_t *>(input)));
   const uint16_t input_utf8_end_of_code_point_mask =
       utf8_end_of_code_point_mask & 0xFFF;
   if(((utf8_end_of_code_point_mask & 0xFFFF) == 0xFFFF)) {
     // We process the data in chunks of 16 bytes.
-    vec_mergel(in, );
-    vec_vsx_st(vec_mergel(in, vec_splat_u16(0)), 0, reinterpret_cast<vecu8 *>(utf16_output));
-    vec_vsx_st(vec_mergeh(in, vec_splat_u16(0)), 0, reinterpret_cast<vecu8 *>(utf16_output + 8));
+    vec_vsx_st(vec_mergel(in, vec_splat_u8(0)), 0, reinterpret_cast<vecu8 *>(utf16_output));
+    vec_vsx_st(vec_mergeh(in, vec_splat_u8(0)), 0, reinterpret_cast<vecu8 *>(utf16_output + 8));
     utf16_output += 16; // We wrote 16 16-bit characters.
     return 16; // We consumed 16 bytes.
   }
@@ -25,31 +24,31 @@ size_t convert_masked_utf8_to_utf16(const char *input,
     // We want to take 8 2-byte UTF-8 words and turn them into 8 2-byte UTF-16 words.
     // There is probably a more efficient sequence, but the following might do.
     const vecu8 sh = {1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14};
-    const vecu8 perm = vec_perm(in, in, sh);
-    const vecu8 ascii = vec_and(perm, vec_splat_u16(0x7f));
+    const vecu16 perm = reinterpret_cast<vecu16>(vec_perm(in, in, sh));
+    const vecu16 ascii = vec_and(perm,vec_splat_u16(0x7f));
     const vecu16 highbyte = vec_and(perm, vec_splat_u16(0x1f00));
-    const vecu16 composed = vec_or(ascii, vec_srl(highbyte, 2));
-    vec_vsx_st(composed, 0, reinterpret_cast<vecu8 *>(utf16_output));
+    const vecu16 composed = vec_or(ascii, vec_srl(highbyte, vec_splat_u16(2)));
+    vec_vsx_st(composed, 0, reinterpret_cast<vecu16 *>(utf16_output));
     utf16_output += 8; // We wrote 16 bytes, 8 code points.
     return 16;
   }
   if(input_utf8_end_of_code_point_mask == 0x924) {
     // We want to take 4 3-byte UTF-8 words and turn them into 4 2-byte UTF-16 words.
     // There is probably a more efficient sequence, but the following might do.
-    const vecu8 sh = {2, 1, 0, -1, 5, 4, 3, -1, 8, 7, 6, -1, 11, 10, 9, -1};
-    const vecu8 perm = vec_perm(in, in, sh);
+    const vecu8 sh = {2, 1, 0, 127, 5, 4, 3, 127, 8, 7, 6, 127, 11, 10, 9, 127}; // the 127 become 15, we use them to mark unwanted values.
+    const vecu32 perm = reinterpret_cast<vecu32>(vec_perm(in, in, sh));
     const vecu32 ascii =
         vec_and(perm, vec_splat_u32(0x7f)); // 7 or 6 bits
     const vecu32 middlebyte =
         vec_and(perm, vec_splat_u32(0x3f00)); // 5 or 6 bits
-    const vecu32 middlebyte_shifted = vec_srl(middlebyte, 2);
+    const vecu32 middlebyte_shifted = vec_srl(middlebyte, vec_splat_u32(2));
     const vecu32 highbyte =
         vec_and(perm, vec_splat_u32(0x0f0000)); // 4 bits
-    const vecu32 highbyte_shifted = vec_srl(highbyte, 4);
+    const vecu32 highbyte_shifted = vec_srl(highbyte, vec_splat_u32(4));
     const vecu32 composed =
         vec_or(vec_or(ascii, middlebyte_shifted), highbyte_shifted);
-    const vecu8 composed_repacked = vec_packsu(composed, composed);
-    vec_vsx_st(composed_repacked, 0, reinterpret_cast<vecu8 *>(utf16_output));
+    const vecu16 composed_repacked = reinterpret_cast<vecu16>(vec_packsu(composed, composed));
+    vec_vsx_st(composed_repacked, 0, reinterpret_cast<vecu16 *>(utf16_output));
     utf16_output += 4;
     return 12;
   }
@@ -66,44 +65,44 @@ size_t convert_masked_utf8_to_utf16(const char *input,
     // words spanning between 1 and 2 bytes each is 12 bytes. On processors
     // where pdep/pext is fast, we might be able to use a small lookup table.
     const vecu8 sh = (vecu8)(vec_vsx_ld(0, reinterpret_cast<const uint8_t *>(tables::utf8_to_utf16::shufutf8[idx])));
-    const vecu8 perm = vec_perm(in, in, sh);
+    const vecu16 perm = reinterpret_cast<vecu16>(vec_perm(in, in, sh));
     const vecu16 ascii = vec_and(perm, vec_splat_u16(0x7f));
     const vecu16 highbyte = vec_and(perm, vec_splat_u16(0x1f00));
-    const vecu16 composed = vec_or(ascii, vec_srl(highbyte, 2));
-    vec_vsx_st(composed, 0, reinterpret_cast<vecu8 *>(utf16_output));
+    const vecu16 composed = vec_or(ascii, vec_srl(highbyte, vec_splat_u16(2)));
+    vec_vsx_st(composed, 0, reinterpret_cast<vecu16 *>(utf16_output));
     utf16_output += 6; // We wrote 12 bytes, 6 code points.
   } else if (idx < 145) {
     // FOUR (4) input code-words
     const vecu8 sh = (vecu8)(vec_vsx_ld(0, reinterpret_cast<const uint8_t *>(tables::utf8_to_utf16::shufutf8[idx])));
-    const vecu8 perm = vec_perm(in, in, sh);
+    const vecu32 perm = reinterpret_cast<vecu32>(vec_perm(in, in, sh));
     const vecu32 ascii =
         vec_and(perm, vec_splat_u32(0x7f)); // 7 or 6 bits
     const vecu32 middlebyte =
         vec_and(perm, vec_splat_u32(0x3f00)); // 5 or 6 bits
-    const vecu32 middlebyte_shifted = vec_srl(middlebyte, 2);
+    const vecu32 middlebyte_shifted = vec_srl(middlebyte, vec_splat_u32(2));
     const vecu32 highbyte =
         vec_and(perm, vec_splat_u32(0x0f0000)); // 4 bits
-    const vecu32 highbyte_shifted = vec_srl(highbyte, 4);
+    const vecu32 highbyte_shifted = vec_srl(highbyte, vec_splat_u32(4));
     const vecu32 composed =
         vec_or(vec_or(ascii, middlebyte_shifted), highbyte_shifted);
-    const vecu8 composed_repacked = vec_packsu(composed, composed);
-    vec_vsx_st(composed_repacked, 0, reinterpret_cast<vecu8 *>(utf16_output));
+    const vecu16 composed_repacked = reinterpret_cast<vecu16>(vec_packsu(composed, composed));
+    vec_vsx_st(composed_repacked, 0, reinterpret_cast<vecu16 *>(utf16_output));
     utf16_output += 4;
   } else if (idx < 209) {
     // TWO (2) input code-words
     const vecu8 sh = (vecu8)(vec_vsx_ld(0, reinterpret_cast<const uint8_t *>(tables::utf8_to_utf16::shufutf8[idx])));
-    const vecu8 perm = vec_perm(in, in, sh);
+    const vecu32 perm = reinterpret_cast<vecu32>(vec_perm(in, in, sh));
     const vecu32 ascii = vec_and(perm, vec_splat_u32(0x7f));
     const vecu32 middlebyte = vec_and(perm, vec_splat_u32(0x3f00));
-    const vecu32 middlebyte_shifted = vec_srl(middlebyte, 2);
+    const vecu32 middlebyte_shifted = vec_srl(middlebyte, vec_splat_u32(2));
     vecu32 middlehighbyte = vec_and(perm, vec_splat_u32(0x3f0000));
     // correct for spurious high bit
     const vecu32 correct =
-        vec_srl(vec_and(perm, vec_splat_u32(0x400000)), 1);
+        vec_srl(vec_and(perm, vec_splat_u32(0x400000)), vec_splat_u32(1));
     middlehighbyte = vec_xor(correct, middlehighbyte);
-    const vecu32 middlehighbyte_shifted = vec_srl(middlehighbyte, 4);
+    const vecu32 middlehighbyte_shifted = vec_srl(middlehighbyte, vec_splat_u32(4));
     const vecu32 highbyte = vec_and(perm, vec_splat_u32(0x07000000));
-    const vecu32 highbyte_shifted = vec_srl(highbyte, 6);
+    const vecu32 highbyte_shifted = vec_srl(highbyte, vec_splat_u32(6));
     const vecu32 composed =
         vec_or(vec_or(ascii, middlebyte_shifted),
                      vec_or(highbyte_shifted, middlehighbyte_shifted));
@@ -111,12 +110,12 @@ size_t convert_masked_utf8_to_utf16(const char *input,
         vec_sub(composed, vec_splat_u32(0x10000));
     const vecu32 lowtenbits =
         vec_and(composedminus, vec_splat_u32(0x3ff));
-    const vecu32 hightenbits = vec_srl(composedminus, 10);
+    const vecu32 hightenbits = vec_srl(composedminus, vec_splat_u32(10));
     const vecu32 lowtenbitsadd =
         vec_add(lowtenbits, vec_splat_u32(0xDC00));
     const vecu32 hightenbitsadd =
         vec_add(hightenbits, vec_splat_u32(0xD800));
-    const vecu32 lowtenbitsaddshifted = vec_srl(lowtenbitsadd, 16);
+    const vecu32 lowtenbitsaddshifted = vec_srl(lowtenbitsadd, vec_splat_u32(16));
     const vecu32 surrogates =
         vec_or(hightenbitsadd, lowtenbitsaddshifted);
     uint32_t basic_buffer[4];
