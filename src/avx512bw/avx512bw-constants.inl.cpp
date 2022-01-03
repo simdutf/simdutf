c5c1eeea23512c5ca211805c5989c9994e2bd373
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2022-01-03 22:14:40 +0100

    Add AVX512BW implementation::convert_utf8_to_utf16

--- src/avx512bw/avx512bw-constants.inl.cpp
+++ src/avx512bw/avx512bw-constants.inl.cpp
@@ -18,6 +18,17 @@ const __m512i v_0000_00c0 = _mm512_set1_epi32(0xc0);
 const __m512i v_0000_0080 = _mm512_set1_epi32(0x80);
 const __m512i v_fc00_fc00 = _mm512_set1_epi32(0xfc00fc00);
 const __m512i v_d800_dc00 = _mm512_set1_epi32(0xd800dc00);
+const __m512i v_0f = _mm512_set1_epi8(0x0f);
+const __m512i v_10 = _mm512_set1_epi8(0x10);
+const __m512i v_40 = _mm512_set1_epi8(0x40);
+const __m512i v_80 = _mm512_set1_epi8(char(0x80));
+const __m512i v_1f = _mm512_set1_epi8(0x1f);
+const __m512i v_3f = _mm512_set1_epi8(0x3f);
+const __m512i v_c0 = _mm512_set1_epi8(char(0xc0));
+const __m512i v_c2 = _mm512_set1_epi8(char(0xc2));
+const __m512i v_e0 = _mm512_set1_epi8(char(0xe0));
+const __m512i v_f0 = _mm512_set1_epi8(char(0xf0));
+const __m512i v_f8 = _mm512_set1_epi8(char(0xf8));
 
 const __m512i broadcast_0th_lane = _mm512_setr_epi32(
     0, 1, 2, 3,
