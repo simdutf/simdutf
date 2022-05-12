7eda8b8c92ced593165d1032944d32b1dd811379
maxeljkin <maxeljkin@gmail.com>
2022-05-12 04:12:42 +0300

    fix valid_utf8_to_utf16.h producing invalid utf16

--- src/generic/utf8_to_utf16/valid_utf8_to_utf16.h
+++ src/generic/utf8_to_utf16/valid_utf8_to_utf16.h
@@ -25,12 +25,16 @@ simdutf_warn_unused size_t convert_valid(const char* input, size_t size,
     // involved.
     //
     simd8x64<int8_t> in(reinterpret_cast<const int8_t *>(input + pos));
-    uint64_t utf8_continuation_mask = in.lt(-65 + 1);
-    // -65 is 0b10111111 in two-complement's, so largest possible continuation byte
-    if(utf8_continuation_mask != 0) {
+    if(in.is_ascii()) {
+      in.store_ascii_as_utf16(utf16_output);
+      utf16_output += 64;
+      pos += 64;
+    } else {
       // Slow path. We hope that the compiler will recognize that this is a slow path.
       // Anything that is not a continuation mask is a 'leading byte', that is, the
       // start of a new code point.
+      uint64_t utf8_continuation_mask = in.lt(-65 + 1);
+      // -65 is 0b10111111 in two-complement's, so largest possible continuation byte
       uint64_t utf8_leading_mask = ~utf8_continuation_mask;
       // The *start* of code points is not so useful, rather, we want the *end* of code points.
       uint64_t utf8_end_of_code_point_mask = utf8_leading_mask>>1;
@@ -63,10 +67,6 @@ simdutf_warn_unused size_t convert_valid(const char* input, size_t size,
       // 64-byte block.These bytes will be processed again. So we have an 
       // 80% efficiency (in the worst case). In practice we expect an 
       // 85% to 90% efficiency.
-    } else {
-      in.store_ascii_as_utf16(utf16_output);
-      utf16_output += 64;
-      pos += 64;
     }
   }
   utf16_output += scalar::utf8_to_utf16::convert_valid(input + pos, size - pos, utf16_output);
