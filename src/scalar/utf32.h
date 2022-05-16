e2abe9a20448897a8e8db276f598cd268745f1dd
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-05-16 17:09:43 +0000

    Scalar validation.

--- src/scalar/utf32.h
+++ src/scalar/utf32.h
@@ -36,6 +36,20 @@ inline size_t utf16_length_from_utf32(const char32_t* buf, size_t len) {
   return counter;
 }
 
+inline simdutf_warn_unused bool validate(const char32_t *buf, size_t len) noexcept {
+  const uint32_t *data = reinterpret_cast<const uint32_t *>(buf);
+  uint64_t pos = 0;
+  while (pos < len) {
+    uint32_t word = data[pos];
+    if(word > 0x10FFFF || (word >= 0xD800 && word <= 0xDFFF)) {
+        return false;
+    } else {
+        pos++;
+    }
+  }
+  return true;
+}
+
 } // utf32 namespace
 } // unnamed namespace
 } // namespace scalar
