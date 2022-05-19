0d1e02a2d410731cd5b28cd271c36f052a07948c
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-05-19 20:33:07 +0000

    Add simdutf to benchmark.

--- benchmarks/src/benchmark.cpp
+++ benchmarks/src/benchmark.cpp
@@ -71,7 +71,9 @@ Benchmark::Benchmark(std::vector<input::Testcase>&& testcases)
         {"count_utf8", {simdutf::encoding_type::UTF8}},
         {"count_utf16", {simdutf::encoding_type::UTF16_LE}},
         {"convert_utf8_to_utf16", {simdutf::encoding_type::UTF8}},
+        {"convert_utf8_to_utf32", {simdutf::encoding_type::UTF8}},
         {"convert_utf8_to_utf16_with_dynamic_allocation", {simdutf::encoding_type::UTF8}},
+        {"convert_utf8_to_utf32_with_dynamic_allocation", {simdutf::encoding_type::UTF8}},
         {"convert_valid_utf8_to_utf16", {simdutf::encoding_type::UTF8}},
         {"convert_valid_utf8_to_utf32", {simdutf::encoding_type::UTF8}},
         {"convert_utf16_to_utf8", {simdutf::encoding_type::UTF16_LE}},
@@ -312,8 +314,12 @@ void Benchmark::run(const std::string& procedure_name, size_t iterations) {
         run_count_utf16(*implementation, iterations);
     } else if(name == "convert_utf8_to_utf16") {
         run_convert_utf8_to_utf16(*implementation, iterations);
+    } else if(name == "convert_utf8_to_utf32") {
+        run_convert_utf8_to_utf32(*implementation, iterations);
     } else if(name == "convert_utf8_to_utf16_with_dynamic_allocation") {
         run_convert_utf8_to_utf16_with_dynamic_allocation(*implementation, iterations);
+    } else if(name == "convert_utf8_to_utf32_with_dynamic_allocation") {
+        run_convert_utf8_to_utf32_with_dynamic_allocation(*implementation, iterations);
     } else if(name == "convert_valid_utf8_to_utf16") {
         run_convert_valid_utf8_to_utf16(*implementation, iterations);
     } else if(name == "convert_valid_utf8_to_utf32") {
@@ -389,6 +395,23 @@ void Benchmark::run_convert_utf8_to_utf16(const simdutf::implementation& impleme
 }
 
 
+void Benchmark::run_convert_utf8_to_utf32(const simdutf::implementation& implementation, size_t iterations) {
+    const char*  data = reinterpret_cast<const char*>(input_data.data());
+    const size_t size = input_data.size();
+    std::unique_ptr<char32_t[]> output_buffer{new char32_t[size]};
+    volatile size_t sink{0};
+
+    auto proc = [&implementation, data, size, &output_buffer, &sink]() {
+        sink = implementation.convert_utf8_to_utf32(data, size, output_buffer.get());
+    };
+    count_events(proc, iterations); // warming up!
+    const auto result = count_events(proc, iterations);
+    if((sink == 0) && (size != 0) && (iterations > 0)) { std::cerr << "The output is zero which might indicate an error.\n"; }
+    size_t char_count = active_implementation->count_utf8(data, size);
+    print_summary(result, size, char_count);
+}
+
+
 void Benchmark::run_convert_utf8_to_utf16_with_dynamic_allocation(const simdutf::implementation& implementation, size_t iterations) {
     const char*  data = reinterpret_cast<const char*>(input_data.data());
     const size_t size = input_data.size();
@@ -397,6 +420,23 @@ void Benchmark::run_convert_utf8_to_utf16_with_dynamic_allocation(const simdutf:
         auto dyn_size = implementation.utf16_length_from_utf8(data, size);
         std::unique_ptr<char16_t[]> output_buffer{new char16_t[dyn_size]};
         sink = implementation.convert_utf8_to_utf16(data, size, output_buffer.get());
+    };
+    count_events(proc, iterations); // warming up!
+    const auto result = count_events(proc, iterations);
+    if((sink == 0) && (size != 0) && (iterations > 0)) { std::cerr << "The output is zero which might indicate an error.\n"; }
+    size_t char_count = active_implementation->count_utf8(data, size);
+    print_summary(result, size, char_count);
+}
+
+
+void Benchmark::run_convert_utf8_to_utf32_with_dynamic_allocation(const simdutf::implementation& implementation, size_t iterations) {
+    const char*  data = reinterpret_cast<const char*>(input_data.data());
+    const size_t size = input_data.size();
+    volatile size_t sink{0};
+    auto proc = [&implementation, data, size, &sink]() {
+        auto dyn_size = implementation.utf32_length_from_utf8(data, size);
+        std::unique_ptr<char32_t[]> output_buffer{new char32_t[dyn_size]};
+        sink = implementation.convert_utf8_to_utf32(data, size, output_buffer.get());
     };
     count_events(proc, iterations); // warming up!
     const auto result = count_events(proc, iterations);
