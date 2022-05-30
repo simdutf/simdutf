53e76f3875d044a6de2f09c212ae166a9032b2a3
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-05-30 18:12:06 +0000

    Fix is_zero check.

--- src/scalar/utf32.h
+++ src/scalar/utf32.h
@@ -9,12 +9,10 @@ namespace utf32 {
 inline simdutf_warn_unused bool validate(const char32_t *buf, size_t len) noexcept {
   const uint32_t *data = reinterpret_cast<const uint32_t *>(buf);
   uint64_t pos = 0;
-  while (pos < len) {
+  for(;pos < len; pos++) {
     uint32_t word = data[pos];
     if(word > 0x10FFFF || (word >= 0xD800 && word <= 0xDFFF)) {
         return false;
-    } else {
-        pos++;
     }
   }
   return true;
