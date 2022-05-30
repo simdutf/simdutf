96fb681a35a71b71f3409880ffd085961f6d5ba6
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-05-30 18:32:21 +0000

    Add validation for westmere.

--- src/westmere/sse_validate_utf32le.cpp
+++ src/westmere/sse_validate_utf32le.cpp
@@ -1,49 +1,32 @@
-/*
-    In UTF-16 words in range 0xD800 to 0xDFFF have special meaning.
-
-    In a vectorized algorithm we want to examine the most significant
-    nibble in order to select a fast path. If none of highest nibbles
-    are 0xD (13), than we are sure that UTF-16 chunk in a vector
-    register is valid.
-
-    Let us analyze what we need to check if the nibble is 0xD. The
-    value of the preceding nibble determines what we have:
-
-    0xd000 .. 0xd7ff - a valid word
-    0xd800 .. 0xdbff - low surrogate
-    0xdc00 .. 0xdfff - high surrogate
-
-    Other constraints we have to consider:
-    - there must not be two consecutive low surrogates (0xd800 .. 0xdbff)
-    - there must not be two consecutive high surrogates (0xdc00 .. 0xdfff)
-    - there must not be sole low surrogate nor high surrogate
-
-    We're going to build three bitmasks based on the 3rd nibble:
-    - V = valid word,
-    - L = low surrogate (0xd800 .. 0xdbff)
-    - H = high surrogate (0xdc00 .. 0xdfff)
-
-      0   1   2   3   4   5   6   7    <--- word index
-    [ V | L | H | L | H | V | V | L ]
-      1   0   0   0   0   1   1   0     - V = valid masks
-      0   1   0   1   0   0   0   1     - L = low surrogate
-      0   0   1   0   1   0   0   0     - H high surrogate
-
-
-      1   0   0   0   0   1   1   0   V = valid masks
-      0   1   0   1   0   0   0   0   a = L & (H >> 1)
-      0   0   1   0   1   0   0   0   b = a << 1
-      1   1   1   1   1   1   1   0   c = V | a | b
-                                  ^
-                                  the last bit can be zero, we just consume 7 words
-                                  and recheck this word in the next iteration
-*/
-
 /* Returns:
    - pointer to the last unprocessed character (a scalar fallback should check the rest);
    - nullptr if an error was detected.
 */
 const char32_t* sse_validate_utf32le(const char32_t* input, size_t size) {
+    const char32_t* end = input + size;
+
+    const __m128i standardmax = _mm_set1_epi32(0x10ffff);
+    const __m128i offset = _mm_set1_epi32(0xffff2000);
+    const __m128i standardoffsetmax = _mm_set1_epi32(0xfffff7ff);
+    __m128i currentmax = _mm_setzero_si128();
+    __m128i currentoffsetmax = _mm_setzero_si128();
+
+    while (input + 4 < end) {
+        const __m128i in = _mm_loadu_si128((__m128i *)input);
+        currentmax = _mm_max_epu32(in,currentmax);
+        currentoffsetmax = _mm_max_epu32(_mm_add_epi32(in, offset), currentoffsetmax);
+        input += 4;
+    }
+   
+    __m128i is_zero = _mm_xor_si128(_mm_max_epu32(currentmax, standardmax), standardmax);
+    if(_mm_test_all_zeros(is_zero, is_zero) == 0) {
+        return nullptr;
+    }
+
+    is_zero = _mm_xor_si128(_mm_max_epu32(currentoffsetmax, standardoffsetmax), standardoffsetmax);
+    if(_mm_test_all_zeros(is_zero, is_zero) == 0) {
+        return nullptr;
+    }
 
     return input;
 }
