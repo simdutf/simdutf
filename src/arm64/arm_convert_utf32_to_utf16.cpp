f6f48407cc05d64de3e534c4e0ee89217103cd4a
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-06-07 20:21:13 +0000

    Fix arm.

--- src/arm64/arm_convert_utf32_to_utf16.cpp
+++ src/arm64/arm_convert_utf32_to_utf16.cpp
@@ -1,5 +1,5 @@
-std::pair<const char32_t*, char16_t*> arm_convert_utf32_to_utf16(const char32_t* buf, size_t len, char16_t* utf16_output) {
-  uint8_t * utf8_output = reinterpret_cast<uint8_t*>(utf8_out);
+std::pair<const char32_t*, char16_t*> arm_convert_utf32_to_utf16(const char32_t* buf, size_t len, char16_t* utf16_out) {
+  uint8_t * utf16_output = reinterpret_cast<uint8_t*>(utf16_out);
   const char32_t* end = buf + len;
 
   return std::make_pair(buf, reinterpret_cast<char*>(utf16_output));
