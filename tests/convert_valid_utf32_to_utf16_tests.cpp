bcc73370379fea937df497c5a5f426b6171d61fd
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-06-10 21:10:04 +0000

    Rename utf32 to utf16 tests.

--- tests/convert_valid_utf32_to_utf16_tests.cpp
+++ tests/convert_valid_utf32_to_utf16_tests.cpp
@@ -18,74 +18,65 @@ namespace {
   constexpr int trials = 1000;
 }
 
-TEST(convert_pure_ASCII) {
-  size_t counter = 0;
-  auto generator = [&counter]() -> uint32_t {
-    return counter++ & 0x7f;
-  };
-
-  auto procedure = [&implementation](const char32_t* utf32, size_t size, char16_t* utf16) -> size_t {
-    return implementation.convert_valid_utf32_to_utf16(utf32, size, utf16);
-  };
-
-  std::array<size_t, 5> input_size{16, 12, 64, 128, 256};
-  for (size_t size: input_size) {
-    transcode_utf32_to_utf16_test_base test(generator, size); 
-    ASSERT_TRUE(test(procedure));
-  }
-}
-
-TEST(convert_into_1_or_2_UTF8_bytes) {
-  for(size_t trial = 0; trial < trials; trial ++) {
-    uint32_t seed{1234+uint32_t(trial)};
-    if ((trial % 100) == 0) { std::cout << "."; std::cout.flush(); }
-    simdutf::tests::helpers::RandomInt random(0x0000, 0x07ff, seed); // range for 1 or 2 UTF-8 bytes
-
-    auto procedure = [&implementation](const char32_t* utf32, size_t size, char16_t* utf16) -> size_t {
-      return implementation.convert_valid_utf32_to_utf16(utf32, size, utf16);
-    };
-
-    for (size_t size: input_size) {
-      transcode_utf32_to_utf16_test_base test(random, size);   
-      ASSERT_TRUE(test(procedure));
-    }
-  }
-}
-
-TEST(convert_into_1_or_2_or_3_UTF8_bytes) {
+TEST(convert_into_2_UTF16_bytes) {
   for(size_t trial = 0; trial < trials; trial ++) {
     if ((trial % 100) == 0) { std::cout << "."; std::cout.flush(); }
-    // range for 1, 2 or 3 UTF-8 bytes
-    simdutf::tests::helpers::RandomIntRanges random({{0x0000, 0x007f},
-                                                     {0x0080, 0x07ff},
-                                                     {0x0800, 0xd7ff},
+    // range for 2 UTF-16 bytes
+    simdutf::tests::helpers::RandomIntRanges random({{0x0000, 0xd7ff},
                                                      {0xe000, 0xffff}}, 0);
 
     auto procedure = [&implementation](const char32_t* utf32, size_t size, char16_t* utf16) -> size_t {
-      return implementation.convert_valid_utf32_to_utf16(utf32, size, utf16);
+      return implementation.convert_utf32_to_utf16(utf32, size, utf16);
     };
-
+    auto size_procedure = [&implementation](const char32_t* utf32, size_t size) -> size_t {
+      return implementation.utf16_length_from_utf32(utf32, size);
+    };
     for (size_t size: input_size) {
-      transcode_utf32_to_utf16_test_base test(random, size);  
+      transcode_utf32_to_utf16_test_base test(random, size);
       ASSERT_TRUE(test(procedure));
+      ASSERT_TRUE(test.check_size(size_procedure));    
     }
   }
 }
 
-TEST(convert_into_3_or_4_UTF8_bytes) {
+TEST(convert_into_4_UTF16_bytes) {
   for(size_t trial = 0; trial < trials; trial ++) {
     if ((trial % 100) == 0) { std::cout << "."; std::cout.flush(); }
-    // range for 3 or 4 UTF-8 bytes
-    simdutf::tests::helpers::RandomIntRanges random({{0x0800, 0xd800-1},
-                                                     {0xe000, 0x10ffff}}, 0);
+    // range for 4 UTF-16 bytes
+    simdutf::tests::helpers::RandomIntRanges random({{0x10000, 0x10ffff}}, 0);
 
     auto procedure = [&implementation](const char32_t* utf32, size_t size, char16_t* utf16) -> size_t {
-      return implementation.convert_valid_utf32_to_utf16(utf32, size, utf16);
+      return implementation.convert_utf32_to_utf16(utf32, size, utf16);
     };
-
+    auto size_procedure = [&implementation](const char32_t* utf32, size_t size) -> size_t {
+      return implementation.utf16_length_from_utf32(utf32, size);
+    };
     for (size_t size: input_size) {
-      transcode_utf32_to_utf16_test_base test(random, size);  
+      transcode_utf32_to_utf16_test_base test(random, size);
       ASSERT_TRUE(test(procedure));
+      ASSERT_TRUE(test.check_size(size_procedure));    
+    }
+  }
+}
+
+TEST(convert_into_2_or_4_UTF16_bytes) {
+  for(size_t trial = 0; trial < trials; trial ++) {
+    if ((trial % 100) == 0) { std::cout << "."; std::cout.flush(); }
+    // range for 2 or 4 UTF-16 bytes (all codepoints)
+    simdutf::tests::helpers::RandomIntRanges random({{0x0000, 0xd7ff},
+                                                     {0xe000, 0xffff},
+                                                     {0x10000, 0x10ffff}}, 0);
+
+    auto procedure = [&implementation](const char32_t* utf32, size_t size, char16_t* utf16) -> size_t {
+      return implementation.convert_utf32_to_utf16(utf32, size, utf16);
+    };
+    auto size_procedure = [&implementation](const char32_t* utf32, size_t size) -> size_t {
+      return implementation.utf16_length_from_utf32(utf32, size);
+    };
+    for (size_t size: input_size) {
+      transcode_utf32_to_utf16_test_base test(random, size);
+      ASSERT_TRUE(test(procedure));
+      ASSERT_TRUE(test.check_size(size_procedure));    
     }
   }
 }
