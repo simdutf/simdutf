8778935d1ef802be6ac554943449fd418caba38d
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-06-13 16:51:02 +0000

    Change haswell to work with 256-bit.

--- src/haswell/avx2_convert_utf32_to_utf16.cpp
+++ src/haswell/avx2_convert_utf32_to_utf16.cpp
@@ -10,10 +10,10 @@ std::pair<const char32_t*, char16_t*> avx2_convert_utf32_to_utf16(const char32_t
     const __m256i v_ffff0000 = _mm256_set1_epi32((int32_t)0xffff0000);
 
     // no bits set above 16th bit <=> can pack to UTF16 without surrogate pairs
-    const __m256i saturation__bytemask = _mm256_cmpeq_epi32(_mm256_and_si256(in, v_ffff0000), v_00000000);
-    const uint32_t saturation__bitmask = static_cast<uint32_t>(_mm256_movemask_epi8(saturation__bytemask));
+    const __m256i saturation_bytemask = _mm256_cmpeq_epi32(_mm256_and_si256(in, v_ffff0000), v_00000000);
+    const uint32_t saturation_bitmask = static_cast<uint32_t>(_mm256_movemask_epi8(saturation_bytemask));
 
-    if (saturation__bitmask == 0xffffffff) {
+    if (saturation_bitmask == 0xffffffff) {
       const __m128i utf16_packed = _mm_packus_epi32(_mm256_castsi256_si128(in),_mm256_extractf128_si256(in,1));
       _mm_storeu_si128((__m128i*)utf16_output, utf16_packed);
       utf16_output += 8;
