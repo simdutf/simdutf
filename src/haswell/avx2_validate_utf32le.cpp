1361441fcaebb452e54a4001fcd96ee1523c10cf
Daniel Lemire <lemire@gmail.com>
2022-06-17 11:17:46 -0400

    Merged.

--- src/haswell/avx2_validate_utf32le.cpp
+++ src/haswell/avx2_validate_utf32le.cpp
@@ -1,49 +1,32 @@
-/*
-    In UTF-16 words in range 0xD800 to 0xDFFF have special meaning.
-
-    In a vectorized algorithm we want to examine the most significant
-    nibble in order to select a fast path. If none of highest nibbles
-    are 0xD (13), than we are sure that UTF-16 chunk in a vector
-    register is valid.
-
-    Let us analyze what we need to check if the nibble is 0xD. The
-    value of the preceding nibble determines what we have:
-
-    0xd000 .. 0xd7ff - a valid word
-    0xd800 .. 0xdbff - low surrogate
-    0xdc00 .. 0xdfff - high surrogate
-
-    Other constraints we have to consider:
-    - there must not be two consecutive low surrogates (0xd800 .. 0xdbff)
-    - there must not be two consecutive high surrogates (0xdc00 .. 0xdfff)
-    - there must not be sole low surrogate nor high surrogate
-
-    We're going to build three bitmasks based on the 3rd nibble:
-    - V = valid word,
-    - L = low surrogate (0xd800 .. 0xdbff)
-    - H = high surrogate (0xdc00 .. 0xdfff)
-
-      0   1   2   3   4   5   6   7    <--- word index
-    [ V | L | H | L | H | V | V | L ]
-      1   0   0   0   0   1   1   0     - V = valid masks
-      0   1   0   1   0   0   0   1     - L = low surrogate
-      0   0   1   0   1   0   0   0     - H high surrogate
-
-
-      1   0   0   0   0   1   1   0   V = valid masks
-      0   1   0   1   0   0   0   0   a = L & (H >> 1)
-      0   0   1   0   1   0   0   0   b = a << 1
-      1   1   1   1   1   1   1   0   c = V | a | b
-                                  ^
-                                  the last bit can be zero, we just consume 7 words
-                                  and recheck this word in the next iteration
-*/
-
 /* Returns:
    - pointer to the last unprocessed character (a scalar fallback should check the rest);
    - nullptr if an error was detected.
 */
 const char32_t* avx2_validate_utf32le(const char32_t* input, size_t size) {
-    
+    const char32_t* end = input + size;
+
+    const __m256i standardmax = _mm256_set1_epi32(0x10ffff);
+    const __m256i offset = _mm256_set1_epi32(0xffff2000);
+    const __m256i standardoffsetmax = _mm256_set1_epi32(0xfffff7ff);
+    __m256i currentmax = _mm256_setzero_si256();
+    __m256i currentoffsetmax = _mm256_setzero_si256();
+
+    while (input + 8 < end) {
+        const __m256i in = _mm256_loadu_si256((__m256i *)input);
+        currentmax = _mm256_max_epu32(in,currentmax);
+        currentoffsetmax = _mm256_max_epu32(_mm256_add_epi32(in, offset), currentoffsetmax);
+        input += 8;
+    }
+
+    __m256i is_zero = _mm256_xor_si256(_mm256_max_epu32(currentmax, standardmax), standardmax);
+    if(_mm256_testz_si256(is_zero, is_zero) == 0) {
+        return nullptr;
+    }
+
+    is_zero = _mm256_xor_si256(_mm256_max_epu32(currentoffsetmax, standardoffsetmax), standardoffsetmax);
+    if(_mm256_testz_si256(is_zero, is_zero) == 0) {
+        return nullptr;
+    }
+
     return input;
 }
