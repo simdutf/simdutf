caf30872e2864916e55485d2ceeea6358c2fd3bf
NicolasJiaxin <nicolasjiaxin@gmail.com>
2022-07-12 18:43:27 +0000

    Working version.

--- src/icelake/icelake-convert-utf16-to-utf32.inl.cpp
+++ src/icelake/icelake-convert-utf16-to-utf32.inl.cpp
@@ -27,10 +27,10 @@ std::pair<const char16_t*, char32_t*> convert_utf16_to_utf32(const char16_t* buf
       // L - bitmask for low surrogates
       const __mmask32 L = _mm512_cmpeq_epi16_mask(_mm512_and_si512(in, v_fc00), v_d800);
       // H - bitmask for high surrogates
-      const __mmask32 H = _kand_mask32(_knot_mask32(L), S); // H = ~L & S
-      const __mmask32 V = _kxor_mask32(L, _kshiftri_mask32(H, 1));  // V = L ^ (H >> 1)
+      const __mmask32 H = ~L & S;
+      const __mmask32 V = (L ^ (H >> 1)) | (H ^ (L << 1));
                                                                     // A low surrogate must be followed by high one and a high one must be preceded by a low one.
-                                                                    // If valid, V should be equal to 0, except if a low surrogate is the last word of the chunk.
+                                                                    // If valid, V should be equal to 0, except if a surrogate is the last word of the chunk.
                                                                     // This is special (valid) case that we need to handle.
 
       if(V == 0 || V == 0x80000000) {
@@ -50,17 +50,7 @@ std::pair<const char16_t*, char32_t*> convert_utf16_to_utf32(const char16_t* buf
         /*  1. Shift whole register by 16 bits (one word) to the right
             |????.????.????.????|0000.00aa.aaaa.aaaa|
         */
-        const __m512i shift_right = _mm512_setr_epi64(
-            0x0908070605040302,
-            0x11100f0e0d0c0b0a,
-            0x1918171615141312,
-            0x21201f1e1d1c1b1a,
-            0x2928272625242322,
-            0x31302f2e2d2c2b2a,
-            0x3938373635343332,
-            0x80803f3e3d3c3b3a
-        );
-        const __m512i shifted = _mm512_shuffle_epi8(no_prefix, shift_right);
+        const __m512i shifted = _mm512_and_si512(v_0000_03ff, _mm512_loadu_si512((__m512i*)(buf+1)));
 
         /*  2. Expand all words in no_prefix and shifted to 32-bit words
             no_prefix |0000.0000.0000.0000.0000.00aa.aaaa.aaaa|0000.0000.0000.0000.0000.00bb.bbbb.bbbb|
@@ -75,24 +65,24 @@ std::pair<const char16_t*, char32_t*> convert_utf16_to_utf32(const char16_t* buf
             |0000.0000.0000.0000.0000.00aa.aaaa.aaaa|0000.0000.0000.bbbb.bbbb.bb00.0000.0000|
         */
         const __m512i aligned_first = _mm512_mask_slli_epi32(first, (__mmask16)L, first, 10);
-        const __m512i aligned_second = _mm512_mask_slli_epi32(second, (__mmask16)(_kshiftri_mask32(L, 16)), second, 10);
+        const __m512i aligned_second = _mm512_mask_slli_epi32(second, (__mmask16)(L>>16), second, 10);
 
         /*  4. Join fields A and B in lower 32-bit word (lower surrogate position)
             |0000.0000.0000.0000.0000.00aa.aaaa.aaaa|0000.0000.0000.bbbb.bbbb.bbaa.aaaa.aaaa|
         */
         const __m512i joined_first = _mm512_mask_or_epi32(aligned_first, (__mmask16)L, shifted_first, aligned_first);
-        const __m512i joined_second = _mm512_mask_or_epi32(aligned_second, (__mmask16)(_kshiftri_mask32(L, 16)), shifted_second, aligned_second);
+        const __m512i joined_second = _mm512_mask_or_epi32(aligned_second, (__mmask16)(L>>16), shifted_second, aligned_second);
 
         //  5. Add offset 0x10000 to lower 32-bit word to obtain valid UTF-32
         const __m512i v_0001_0000 = _mm512_set1_epi32((uint32_t)0x10000);
         const __m512i utf32_first = _mm512_mask_add_epi32(joined_first, (__mmask16)L, joined_first, v_0001_0000);
-        const __m512i utf32_second = _mm512_mask_add_epi32(joined_second, (__mmask16)(_kshiftri_mask32(L, 16)), joined_second, v_0001_0000);
+        const __m512i utf32_second = _mm512_mask_add_epi32(joined_second, (__mmask16)(L>>16), joined_second, v_0001_0000);
 
         //  6. Store all valid UTF-32 words (only high surrogate positions are invalid)
-        _mm512_mask_compressstoreu_epi32((__m512i *)utf32_output, (__mmask16)(_knot_mask32(H)), utf32_first);
-        utf32_output += count_ones(_cvtmask16_u32((__mmask16)_knot_mask32(H)));
-        _mm512_mask_compressstoreu_epi32((__m512i *)utf32_output, (__mmask16)(_kshiftri_mask32(_knot_mask32(H), 16)), utf32_second);
-        utf32_output += count_ones(_cvtmask32_u32(_kshiftri_mask32(_knot_mask32(H),16)));
+        _mm512_mask_compressstoreu_epi32((__m512i *)utf32_output, (__mmask16)(~H), utf32_first);
+        utf32_output += count_ones((uint16_t)(~H));
+        _mm512_mask_compressstoreu_epi32((__m512i *)utf32_output, (__mmask16)((~H)>>16), utf32_second);
+        utf32_output += count_ones((~H)>>16);
 
         buf += 32;
         // If last word in the chunk is a lone a surrogate, we process it in the next iteration to check if valid
