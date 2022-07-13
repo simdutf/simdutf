45f5376fbc7b105c39a04b1d4b6bdcb90ef00550
Daniel Lemire <lemire@gmail.com>
2022-07-13 14:34:39 -0400

    Fixing VS usage.

--- src/icelake/icelake-utf8-common.inl.cpp
+++ src/icelake/icelake-utf8-common.inl.cpp
@@ -7,7 +7,7 @@ using utf8_to_utf32_result = std::pair<const char*, uint32_t*>;
 // See: http://0x80.pl/notesen/2021-12-22-test-and-clear-bit.html
 bool test_and_clear_bit(uint32_t& val, int bitpos) {
 #if SIMDUTF_REGULAR_VISUAL_STUDIO
-    return _bittestandreset(val, bitpos);
+    return _bittestandreset(&val, bitpos);
 #else
     /* Non-Microsoft C/C++-compatible compiler, assumes that it supports inline
     * assembly */
