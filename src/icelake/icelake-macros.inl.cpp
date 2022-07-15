83b40e3dfd1b7a2375916f294416f4c3e2711634
Daniel Lemire <lemire@daniel.me>
2022-07-15 16:08:41 +0000

    Moving functions where they logically belong.

--- src/icelake/icelake-macros.inl.cpp
+++ src/icelake/icelake-macros.inl.cpp
@@ -82,35 +82,6 @@
             }                                                                                                \
         }
 
-simdutf_really_inline __m512i expand_and_identify(__m512i lane0, __m512i lane1, int &count) {
-    const __m512i merged = _mm512_mask_mov_epi32(lane0, 0x1000, lane1);
-    const __m512i expand_ver2 = _mm512_setr_epi64(
-                0x0403020103020100,
-                0x0605040305040302,
-                0x0807060507060504,
-                0x0a09080709080706,
-                0x0c0b0a090b0a0908,
-                0x0e0d0c0b0d0c0b0a,
-                0x000f0e0d0f0e0d0c,
-                0x0201000f01000f0e
-    );
-    const __m512i input = _mm512_shuffle_epi8(merged, expand_ver2);
-    const __m512i v_0000_00c0 = _mm512_set1_epi32(0xc0);
-    const __m512i t0 = _mm512_and_si512(input, v_0000_00c0);
-    const __m512i v_0000_0080 = _mm512_set1_epi32(0x80);
-    const __mmask16 leading_bytes = _mm512_cmpneq_epu32_mask(t0, v_0000_0080);
-    count = static_cast<int>(count_ones(leading_bytes));
-    return  _mm512_mask_compress_epi32(_mm512_setzero_si512(), leading_bytes, input);
-}
-
-simdutf_really_inline __m512i expand_utf8_to_utf32(__m512i input) {
-    __m512i char_class = _mm512_srli_epi32(input, 4);
-    /*  char_class = ((input >> 4) & 0x0f) | 0x80808000 */
-    const __m512i v_0000_000f = _mm512_set1_epi32(0x0f);
-    const __m512i v_8080_8000 = _mm512_set1_epi32(0x80808000);
-    char_class = _mm512_ternarylogic_epi32(char_class, v_0000_000f, v_8080_8000, 0xea);
-    return expanded_utf8_to_utf32(char_class, input);
-}
 
 #define SIMDUTF_ICELAKE_WRITE_UTF16_OR_UTF32(INPUT, VALID_COUNT)                                    \
 {                                                                                                   \
