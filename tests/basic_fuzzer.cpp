e439de1069b2ad00fb19ea4e076f1eab76f8af39
Nicolas Boyer <nicolasjiaxin@gmail.com>
2022-08-12 17:32:12 +0000

    Extend basic fuzzer to include transcoding from and to utf32

--- tests/basic_fuzzer.cpp
+++ tests/basic_fuzzer.cpp
@@ -21,7 +21,7 @@ std::array<size_t, 10> input_size{7, 16, 12, 64, 67, 128, 129, 256, 1024, 1025};
 struct buffer {
   std::vector<char> input;
   std::vector<char> output;
-  buffer(size_t size) : input(size), output(2 * size) {}
+  buffer(size_t size) : input(size), output(4 * size) {}
   void print_input() const {
     for (size_t i = 0; i < input.size(); i++) {
       printf("%x ", uint8_t(input[i]));
@@ -55,24 +55,48 @@ TEST(basic_fuzz) {
       bool is_ok_utf16 = implementation.validate_utf16le(
           reinterpret_cast<char16_t *>(buf.input.data()),
           buf.input.size() / sizeof(char16_t));
-      size_t sizeutf8 = implementation.convert_utf8_to_utf16le(
+      bool is_ok_utf32 = implementation.validate_utf32(
+          reinterpret_cast<char32_t *>(buf.input.data()),
+          buf.input.size() / sizeof(char32_t));
+      size_t utf8_to_utf16 = implementation.convert_utf8_to_utf16le(
           buf.input.data(), buf.input.size(),
           reinterpret_cast<char16_t *>(buf.output.data()));
-      size_t sizeutf16 = implementation.convert_utf16le_to_utf8(
+      size_t utf8_to_utf32 = implementation.convert_utf8_to_utf32(
+          buf.input.data(), buf.input.size(),
+          reinterpret_cast<char32_t *>(buf.output.data()));
+      size_t utf16_to_utf8 = implementation.convert_utf16le_to_utf8(
           reinterpret_cast<char16_t *>(buf.input.data()),
           buf.input.size() / sizeof(char16_t), buf.output.data());
-      if(is_ok_utf8 ? sizeutf8 == 0 : sizeutf8 > 0) {
+      size_t utf16_to_utf32 = implementation.convert_utf16le_to_utf32(
+          reinterpret_cast<char16_t *>(buf.input.data()),
+          buf.input.size() / sizeof(char16_t), reinterpret_cast<char32_t *>(buf.output.data()));
+      size_t utf32_to_utf8 = implementation.convert_utf32_to_utf8(
+          reinterpret_cast<char32_t *>(buf.input.data()),
+          buf.input.size() / sizeof(char32_t), buf.output.data());
+      size_t utf32_to_utf16 = implementation.convert_utf32_to_utf16le(
+          reinterpret_cast<char32_t *>(buf.input.data()),
+          buf.input.size() / sizeof(char32_t), reinterpret_cast<char16_t *>(buf.output.data()));
+      if(is_ok_utf8 ? (utf8_to_utf16 == 0 || utf8_to_utf32 == 0) : (utf8_to_utf16 > 0 || utf8_to_utf32 > 0)) {
         std::cout << (is_ok_utf8 ? "UTF-8 is ok" : "UTF-8 is not ok") << std::endl;
         std::cout << " size = " << buf.input.size() << std::endl;
-        std::cout << "  implementation.convert_utf8_to_utf16 return " << sizeutf8 << std::endl;
+        std::cout << "  implementation.convert_utf8_to_utf16 return " << utf8_to_utf16 << std::endl;
+        std::cout << "  implementation.convert_utf8_to_utf32 return " << utf8_to_utf32 << std::endl;
       }
-      ASSERT_TRUE(is_ok_utf8 ? sizeutf8 > 0 : sizeutf8 == 0);
-      if(is_ok_utf16 ? sizeutf16 == 0 : sizeutf16 > 0) {
+      ASSERT_TRUE(is_ok_utf8 ? (utf8_to_utf16 > 0 && utf8_to_utf32 > 0) : (utf8_to_utf16 == 0 && utf8_to_utf32 == 0));
+      if(is_ok_utf16 ? (utf16_to_utf8 == 0 || utf16_to_utf32 == 0) : (utf16_to_utf8 > 0 || utf16_to_utf32 > 0)) {
         std::cout << (is_ok_utf8 ? "UTF-16 is ok" : "UTF-16 is not ok") << std::endl;
-        std::cout << " size = " << buf.input.size() << std::endl;
-        std::cout << "  implementation.convert_utf16_to_utf8 return " << sizeutf16 << std::endl;
+        std::cout << " size = " << buf.input.size() / sizeof(char16_t) << std::endl;
+        std::cout << "  implementation.convert_utf16_to_utf8 return " << utf16_to_utf8 << std::endl;
+        std::cout << "  implementation.convert_utf16_to_utf32 return " << utf16_to_utf32 << std::endl;
       }
-      ASSERT_TRUE(is_ok_utf16 ? sizeutf16 > 0 : sizeutf16 == 0);
+      ASSERT_TRUE(is_ok_utf16 ? (utf16_to_utf8 > 0 && utf16_to_utf32 > 0) : (utf16_to_utf8 == 0 && utf16_to_utf32 == 0));
+      if(is_ok_utf32 ? (utf32_to_utf8 == 0 || utf32_to_utf16 == 0) : (utf32_to_utf8 > 0 || utf32_to_utf16 > 0)) {
+        std::cout << (is_ok_utf8 ? "UTF-16 is ok" : "UTF-16 is not ok") << std::endl;
+        std::cout << " size = " << buf.input.size() / sizeof(char32_t) << std::endl;
+        std::cout << "  implementation.convert_utf32_to_utf8 return " << utf32_to_utf8 << std::endl;
+        std::cout << "  implementation.convert_utf32_to_utf16 return " << utf32_to_utf16 << std::endl;
+      }
+      ASSERT_TRUE(is_ok_utf32 ? (utf32_to_utf8 > 0 && utf32_to_utf16 > 0) : (utf32_to_utf8 == 0 && utf32_to_utf16 == 0));
     }
   }
 }
