6ff928bff6c6cc99d8bf190418d856b6934bcbb7
Robert Clausecker <fuz@fuzs.su>
2022-11-02 15:38:22 +0000

    utf8_to_utf16le_avx512i.c: port UTF-8->UTF-16 kernel to C w/ intrinsics
    Doesn't work yet.

--- utf8_test.c
+++ utf8_test.c
@@ -5,9 +5,11 @@
 
 extern size_t utf8_to_utf16le_ref(char16_t out[restrict], const unsigned char in[restrict], size_t len, size_t *outlen);
 extern size_t utf8_to_utf16le_avx512(char16_t out[restrict], const unsigned char in[restrict], size_t len, size_t *outlen);
+extern size_t utf8_to_utf16le_avx512i(char16_t out[restrict], const unsigned char in[restrict], size_t len, size_t *outlen);
 
 extern size_t utf8_to_utf16le_buflen_ref(size_t);
 extern size_t utf8_to_utf16le_buflen_avx512(size_t);
+extern size_t utf8_to_utf16le_buflen_avx512i(size_t);
 
 //extern size_t utf8_validate_ref(const char16_t in[restrict], size_t len);
 //extern size_t utf18_validate_avx512(const char16_t in[restrict], size_t len);
@@ -103,9 +105,10 @@ void print_utf16(const char16_t *str, size_t len)
 
 int test(int i, const char *vector)
 {
-	char16_t *refbuf, *avx512buf;
-	size_t reflen, avx512len, inlen, refvalid, avx512valid;
-	size_t refxlat, avx512xlat, refout = -1, avx512out = -1, avx512iout = -1;
+	char16_t *refbuf, *avx512buf, *avx512ibuf;
+	size_t reflen, avx512len, avx512ilen, inlen, refvalid, avx512valid;
+	size_t refxlat, avx512xlat, avx512ixlat;
+	size_t refout = -1, avx512out = -1, avx512iout = -1;
 	int result = 0;
 
 	printf("TESTCASE %d\n", i);
@@ -113,6 +116,7 @@ int test(int i, const char *vector)
 	inlen = strlen_ff(vector);
 	reflen = utf8_to_utf16le_buflen_ref(inlen);
 	avx512len = utf8_to_utf16le_buflen_avx512(inlen);
+	avx512ilen = utf8_to_utf16le_buflen_avx512i(inlen);
 
 	refbuf = malloc((reflen + 1) * sizeof *refbuf);
 	if (refbuf == NULL) {
@@ -123,17 +127,26 @@ int test(int i, const char *vector)
 	avx512buf = malloc((avx512len + 1) * sizeof *refbuf);
 	if (avx512buf == NULL) {
 		perror("malloc(avx512buf)");
-		free(refbuf);
+		free(avx512buf);
+		return (1);
+	}
+
+	avx512ibuf = malloc((avx512ilen + 1) * sizeof *refbuf);
+	if (avx512ibuf == NULL) {
+		perror("malloc(avx512ibuf)");
+		free(avx512ibuf);
 		return (1);
 	}
 
 	/* write bullshit to buffers so it's easy to catch uninitialised memory */
-	memset(refbuf, 0xff, reflen);
-	memset(avx512buf, 0xff, avx512len);
+	memset(refbuf, 0xff, reflen * sizeof *refbuf);
+	memset(avx512buf, 0xff, avx512len * sizeof *avx512buf);
+	memset(avx512ibuf, 0xff, avx512ilen * sizeof *avx512ibuf);
 
 	/* add sentinels to catch buffer overruns */
 	refbuf[reflen] = 0xfffe;
 	avx512buf[avx512len] = 0xfffe;
+	avx512ibuf[avx512ilen] = 0xfffe;
 
 #if 0
 	/* check for validation failure */
@@ -151,12 +164,13 @@ int test(int i, const char *vector)
 
 	refxlat = utf8_to_utf16le_ref(refbuf, vector, inlen, &refout);
 	avx512xlat = utf8_to_utf16le_avx512(avx512buf, vector, inlen, &avx512out);
+	avx512ixlat = utf8_to_utf16le_avx512i(avx512ibuf, vector, inlen, &avx512iout);
 
-	if (refxlat != avx512xlat || refout != avx512out) {
+	if (refxlat != avx512xlat || refout != avx512out || refxlat != avx512ixlat || refout != avx512iout) {
 		print_vector(i, vector);
 		printf("length mismatch:\n");
-		printf("	converted: %zu (ref) vs %zu (avx512)\n", refxlat, avx512xlat);
-		printf("	output length: %zu (ref) vs %zu (avx512)\n", refout, avx512out);
+		printf("	converted: %zu (ref) vs %zu (avx512) vs %zu (avx512i)\n", refxlat, avx512xlat, avx512ixlat);
+		printf("	output length: %zu (ref) vs %zu (avx512) vx %zu (avx512i)\n", refout, avx512out, avx512iout);
 
 		goto failed;
 	}
@@ -167,7 +181,7 @@ int test(int i, const char *vector)
 		goto failed;
 	}
 
-	if (memcmp(refbuf, avx512buf, refout) != 0) {
+	if (memcmp(refbuf, avx512buf, refout) != 0 || memcmp(refbuf, avx512ibuf, refout) != 0) {
 		print_vector(i, vector);
 		printf("encoding mismatch\n");
 
@@ -177,21 +191,27 @@ int test(int i, const char *vector)
 		if (avx512out > avx512len)
 			avx512out = avx512len;
 
+		if (avx512iout > avx512ilen)
+			avx512iout = avx512ilen;
+
 		printf("OUTPUT %d/ref (%zu)\n", i, refout);
 		print_utf16(refbuf, refout);
 		printf("OUTPUT %d/avx512 (%zu)\n", i, avx512out);
 		print_utf16(avx512buf, avx512out);
+		printf("OUTPUT %d/avx512i (%zu)\n", i, avx512iout);
+		print_utf16(avx512ibuf, avx512iout);
 
 		result = 1;
 	}
 
-	if (refbuf[reflen] != 0xfffe || avx512buf[avx512len] != 0xfffe) {
+	if (refbuf[reflen] != 0xfffe || avx512buf[avx512len] != 0xfffe || avx512ibuf[avx512ilen] != 0xfffe) {
 		printf("BUFFER OVERRUN (%d)\n", i);
 		abort();
 	}
 
 end:	free(refbuf);
 	free(avx512buf);
+	free(avx512ibuf);
 
 	return (result);
 }
---
+++ utf8_to_utf16le_avx512i.c
@@ -0,0 +1,183 @@
+#include <uchar.h>
+#include <immintrin.h>
+#include <stdint.h>
+
+extern size_t
+utf8_to_utf16le_avx512i(char16_t outbuf[restrict],
+    const unsigned char inbuf[restrict], size_t inlen, size_t *outlen)
+{
+	char16_t *restrict orig_outbuf = outbuf;
+	const unsigned char *restrict orig_inbuf = inbuf;
+	__mmask64 b = _cvtu64_mask64(0xffffffffffffffff);
+
+	while (inlen > 0) {
+		__m512i in, wstripped, P, Wend, Wminus1, Wminus2, Wsum, Wout;
+		__m512i Wshifted, Wd800;
+		__mmask64 m1, m1234, m234, m3, m34, m4, mc1c2, mc, mprocessed;
+		__mmask64 mend, mout, mplus3;
+		__mmask32 Mlo, Mhi, M3, M_U800, M3s, M4s, Mout;
+		size_t nin, nout;
+
+		if (inlen >= 64) {
+			in = _mm512_loadu_epi8(inbuf);
+
+			m1 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0x80), _MM_CMPINT_LT);
+			if (_ktestc_mask32_u8(m1, m1)) { /* all ASCII? */
+				__m512i out;
+
+				out = _mm512_cvtepu8_epi16(_mm512_castsi512_si256(in));
+				_mm512_storeu_epi8(outbuf, out);
+
+				inbuf += 32;
+				inlen -= 32;
+				outbuf += 64;
+				continue;
+			}
+
+			m234 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0xc0), _MM_CMPINT_NLT);
+			m34 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0xe0), _MM_CMPINT_NLT);
+			mc1c2 = _mm512_mask_cmp_epu8_mask(m234, in, _mm512_set1_epi8(0xc2), _MM_CMPINT_LT);
+			if (_ktestz_mask64_u8(mc1c2, mc1c2)) { /* C1/C2 present? */
+			has_c1c2:
+				inlen = _tzcnt_u64(mc1c2);
+				goto tail;
+			}
+
+			m1234 = m1 | m234;
+			if (_ktestz_mask64_u8(m34, m34)) { /* all 1/2 byte? */
+				__m512i minusc2, first, second, out;
+				size_t not_processed;
+
+				mend = ~m234;
+				minusc2 = _mm512_maskz_sub_epi8(m234, in, _mm512_set1_epi8(0xc2));
+				first = _mm512_cvtepi8_epi16(_mm512_castsi512_si256(
+				    _mm512_maskz_compress_epi8(m1234, minusc2)));
+				second = _mm512_cvtepi8_epi16(_mm512_castsi512_si256(
+				    _mm512_maskz_compress_epi8(mend, in)));
+				out = _mm512_add_epi16(second, _mm512_slli_epi16(first, 6));
+				_mm512_storeu_epi16(outbuf, out);
+
+				/* check for sequencing errors */
+				if (mend + mend + 1 != m1234) {
+					uint64_t mismatch, lead_before;
+
+					mismatch = mend + mend + 1 ^ m1234;
+					inlen = _tzcnt_u64(mismatch);
+					/* lead byte where follow byte expected? */
+					if (m1234 & 1 << inlen) {
+						lead_before = m1234 & (1 << inlen) - 1;
+						inlen = 64 - _lzcnt_u64(lead_before);
+					}
+
+					goto tail;
+				}
+
+				mout = _cvtu64_mask64(_pdep_u64(0xffffffff, (uint64_t)mend));
+				not_processed = _lzcnt_u64((uint64_t)mout);
+				inbuf += 64 - not_processed;
+				inlen -= 64 - not_processed;
+				outbuf += 64;
+				continue;
+			}
+		} else { /* handle tail bytes: no fast paths, expedited processing. */
+		tail:	b = _cvtu64_mask64((1 << inlen) -1);
+			in = _mm512_maskz_loadu_epi8(b, inbuf);
+			m1 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0x80), _MM_CMPINT_LT);
+			m234 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0xc0), _MM_CMPINT_NLT);
+			m34 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0xe0), _MM_CMPINT_NLT);
+			m1234 = m1 | m234;
+
+			mc1c2 = _mm512_mask_cmp_epu8_mask(m234, in, _mm512_set1_epi8(0xc2), _MM_CMPINT_LT);
+			if (_ktestz_mask64_u8(mc1c2, mc1c2)) /* C1/C2 present? */
+				goto has_c1c2;
+		}
+
+		m4 = _mm512_cmp_epu8_mask(in, _mm512_set1_epi8(0xf0), _MM_CMPINT_NLT);
+		mplus3 = m4 << 3;
+		mend = (mplus3 | m1234) >> 1 | mplus3;
+
+		wstripped = _mm512_andnot_epi32(_mm512_maskz_mov_epi8(~m1, _mm512_set1_epi8(0xc0)), in);
+		P = _mm512_cvtepu8_epi16(_mm512_castsi512_si256(_mm512_maskz_compress_epi8(mend, _mm512_set_epi8(
+		    63, 62, 61, 60, 59, 58, 57, 56,
+		    55, 54, 53, 52, 51, 50, 49, 48,
+		    47, 46, 45, 44, 43, 42, 41, 40,
+		    39, 38, 37, 36, 35, 34, 33, 32,
+		    31, 30, 29, 28, 27, 26, 25, 24,
+		    23, 22, 21, 20, 19, 18, 17, 16,
+		    15, 14, 13, 12, 11, 10,  9,  8,
+		     7,  6,  5,  4,  3,  2,  1,  0))));
+
+		Wend =_mm512_maskz_permutexvar_epi8(_cvtu64_mask64(0x5555555555555555), P, wstripped);
+		Wminus1 = _mm512_maskz_permutexvar_epi8(_cvtu64_mask64(0x5555555555555555),
+		    _mm512_add_epi8(P, _mm512_set1_epi8(0xff)), _mm512_maskz_mov_epi8(~m1 >> 1, wstripped));
+		Wminus2 = _mm512_maskz_permutexvar_epi8(_cvtu64_mask64(0x5555555555555555),
+		    _mm512_add_epi8(P, _mm512_set1_epi8(0xfe)), _mm512_maskz_mov_epi8(
+		    m34 & _cvtu64_mask64(0x3fffffffffffffff), wstripped));
+		Wsum = _mm512_or_epi32(_mm512_slli_epi16(Wminus2, 12),
+		    _mm512_or_epi32(_mm512_slli_epi16(Wminus1, 6), Wend));
+
+
+		mc = (m234 << 1) | (m34 << 2) | mplus3;
+		if (mc != ~m1234) { /* are continuation bytes where they should be? */
+			uint64_t mismatch, lead_before;
+
+			mismatch = mc ^ ~m1234;
+			inlen = _tzcnt_u64(mismatch);
+
+			/* lead byte where follow byte expected? */
+			if (m1234 & 1 << inlen) {
+				lead_before = m1234 & (1 << inlen) - 1;
+				inlen = 64 - _lzcnt_u64(lead_before);
+			}
+
+			goto tail;
+		}
+
+		Mlo = _cvtu32_mask32(_pext_u64(mplus3, mend));
+		Mhi = Mlo >> 1;
+
+		Wshifted = _mm512_srli_epi16(Wsum, 4);
+		Wout = _mm512_or_epi32(Wsum, _mm512_maskz_mov_epi16(Mlo, _mm512_set1_epi16(0xdc00)));
+		Wout = _mm512_mask_add_epi16(Wout, Mhi, Wshifted, _mm512_set1_epi16(0xd7c0));
+
+		/* check 3 and 4 byte sequences for correctness */
+		m3 = m34 & ~m4;
+		M3 = _cvtu32_mask32(_pext_u64(m3 << 2, mend));
+		/* overlong 3 byte sequence? */
+		M_U800 = M3 & _mm512_cmp_epu16_mask(Wout, _mm512_set1_epi16(0x0800), _MM_CMPINT_LT);
+		Wd800 = _mm512_sub_epi16(Wout, _mm512_set1_epi16(0xd800));
+		/* 3 byte sequence encodes surrogate? */
+		M3s = M3 & _mm512_cmp_epu16_mask(Wd800, _mm512_set1_epi16(0x0800), _MM_CMPINT_LT);
+		/* codepoint > U+10FFFF encoded? 5+ byte sequence encoded? */
+		M4s = Mhi & _mm512_cmp_epu16_mask(Wd800, _mm512_set1_epi16(0x0400), _MM_CMPINT_NLT);
+
+		if (M_U800 | M3s | M4s) {
+			uint64_t where;
+
+			where = M_U800 | M3s | M4s;
+			inlen = _tzcnt_u64(_pdep_u64(where, m1234 | mplus3));
+
+			goto tail;
+		}
+
+		Mout = ~(Mhi & _cvtu32_mask32(0x80000000));
+		mprocessed = _cvtu64_mask64(_pdep_u64((uint64_t)Mout, b & mend));
+		nin = 64 - _lzcnt_u64(mprocessed);
+		nout = _popcnt64(mprocessed);
+
+		_mm512_mask_storeu_epi16(outbuf, _cvtu32_mask32((1 << nout) -1), Wout);
+		inbuf += nin;
+		inlen -= nin;
+		outbuf += nout;
+		continue;
+	}
+
+	*outlen = outbuf - orig_outbuf;
+	return ((size_t)(inbuf - orig_inbuf));
+}
+
+size_t
+utf8_to_utf16le_buflen_avx512i(size_t n)
+{
+	return (n);
+}
