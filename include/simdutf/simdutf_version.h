2534ae7f22794349dadbd3d7a9c9560ece3ff94e
Daniel Lemire <daniel@lemire.me>
2022-12-15 12:10:36 -0500

    Preparing release

--- include/simdutf/simdutf_version.h
+++ include/simdutf/simdutf_version.h
@@ -4,7 +4,7 @@
 #define SIMDUTF_SIMDUTF_VERSION_H
 
 /** The version of simdutf being used (major.minor.revision) */
-#define SIMDUTF_VERSION 2.0.8
+#define SIMDUTF_VERSION 2.0.9
 
 namespace simdutf {
 enum {
@@ -19,7 +19,7 @@ enum {
   /**
    * The revision (major.minor.REVISION) of simdutf being used.
    */
-  SIMDUTF_VERSION_REVISION = 8
+  SIMDUTF_VERSION_REVISION = 9
 };
 } // namespace simdutf
 
