47f54a136343583c88e090a21c518c7fd8e7e3ab
Daniel Lemire <daniel@lemire.me>
2023-01-02 15:41:36 -0500

    minor maintenance for runtime dispatching (#202)
    * Minor maintenance regarding runtime dispatching.
    
    * Minor
    
    * New lines

--- src/simdutf/icelake/begin.h
+++ src/simdutf/icelake/begin.h
@@ -1,5 +1,10 @@
 #define SIMDUTF_IMPLEMENTATION icelake
+
+#if SIMDUTF_CAN_ALWAYS_RUN_ICELAKE
+// nothing needed.
+#else
 SIMDUTF_TARGET_ICELAKE
+#endif
 
 #if SIMDUTF_GCC11ORMORE // workaround for https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105593
 SIMDUTF_DISABLE_GCC_WARNING(-Wmaybe-uninitialized)
