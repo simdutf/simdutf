a2e7ae891360a671cc6bd3b755421c10beb6bc9e
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-03-17 10:12:44 -0700

    feat(fuzz): Adds a roundtrip fuzz harness

---
+++ fuzz/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $SRC/simdutf
+
+cmake -B build
+cmake --build build -j$(nproc)
+
+$CXX $CFLAGS $CXXFLAGS \
+     -I build/singleheader \
+     -c fuzz/roundtrip.cc -o roundtrip.o
+
+$CXX $CFLAGS $CXXFLAGS $LIB_FUZZING_ENGINE roundtrip.o \
+     -o $OUT/roundtrip
