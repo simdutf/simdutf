4bb15e48a69d4030d01b1808a271c8fc89c51fe1
Daniel Lemire <daniel@lemire.me>
2023-04-21 14:23:55 -0400

    Let us run the undefined sanitizers

--- cmake/simdutf-flags.cmake
+++ cmake/simdutf-flags.cmake
@@ -1,9 +1,10 @@
 
 option(SIMDUTF_SANITIZE "Sanitize addresses" OFF)
+option(SIMDUTF_SANITIZE_UNDEFINED "Sanitize undefined behavior" OFF)
 
 if (NOT CMAKE_BUILD_TYPE)
   message(STATUS "No build type selected, default to Release")
-  if(SIMDUTF_SANITIZE)
+  if(SIMDUTF_SANITIZE OR SIMDUTF_SANITIZE_UNDEFINED)
     set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
     # SIMDUTF_SANITIZE only applies to gcc/clang:
     message(STATUS "Setting debug optimization flag to -O1.")
@@ -12,6 +13,8 @@ if (NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
   endif()
 endif()
+
+
 
 set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/tools/cmake")
 
