15cb52a97c0828f787cc572d55a7e5761df54441
Nick Nuon <myjpntrip@gmail.com>
2023-05-19 10:33:50 -0400

    Less verbosity

--- src/scalar/utf32_to_latin1/utf32_to_latin1.h
+++ src/scalar/utf32_to_latin1/utf32_to_latin1.h
@@ -20,12 +20,12 @@ inline size_t convert(const char32_t *buf, size_t len, char *latin1_output) {
 
     while (pos < len) {
         utf32_char = (uint32_t)data[pos]; 
-
+/* 
         printf("This is utf32_char: %02x ", utf32_char);
         printf("This is latin_output: %02x\n", (char)(utf32_char & 0xFF));
         printf("This is utf32_char & 0xFFFFFF00: %02x\n", ((utf32_char & 0xFFFFFF00) == 0));
         printf("This is latin1_output - start: %d\n", latin1_output-start);
-
+ */
         if ((utf32_char & 0xFFFFFF00) == 0) { // Check if the character can be represented in Latin-1
             *latin1_output++ = (char)(utf32_char & 0xFF);
             pos++;
