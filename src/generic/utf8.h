2724a222e74b4c4cb94e178014fbf75eb99fe68a
Nick Nuon <76173566+Nick-Nuon@users.noreply.github.com>
2023-09-25 14:54:57 -0400

    count_code_points change (#322)
    * count_code_points change
    
    * cleanup

--- src/generic/utf8.h
+++ src/generic/utf8.h
@@ -12,12 +12,11 @@ simdutf_really_inline size_t count_code_points(const char* in, size_t size) {
     size_t count = 0;
     for(;pos + 64 <= size; pos += 64) {
       simd8x64<int8_t> input(reinterpret_cast<const int8_t *>(in + pos));
-      uint64_t utf8_continuation_mask = input.lt(-65 + 1);
-      count += 64 - count_ones(utf8_continuation_mask);
+     uint64_t utf8_continuation_mask = input.gt(-65);
+      count += count_ones(utf8_continuation_mask);
     }
     return count + scalar::utf8::count_code_points(in + pos, size - pos);
 }
-
 
 simdutf_really_inline size_t utf16_length_from_utf8(const char* in, size_t size) {
     size_t pos = 0;
