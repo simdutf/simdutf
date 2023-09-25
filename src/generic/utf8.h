c24a29706fffd1c89dc7ab23efa724ff2f07bb55
Daniel Lemire <daniel@lemire.me>
2023-09-25 16:28:57 -0400

    Fixed missing space

--- src/generic/utf8.h
+++ src/generic/utf8.h
@@ -12,7 +12,7 @@ simdutf_really_inline size_t count_code_points(const char* in, size_t size) {
     size_t count = 0;
     for(;pos + 64 <= size; pos += 64) {
       simd8x64<int8_t> input(reinterpret_cast<const int8_t *>(in + pos));
-     uint64_t utf8_continuation_mask = input.gt(-65);
+      uint64_t utf8_continuation_mask = input.gt(-65);
       count += count_ones(utf8_continuation_mask);
     }
     return count + scalar::utf8::count_code_points(in + pos, size - pos);
