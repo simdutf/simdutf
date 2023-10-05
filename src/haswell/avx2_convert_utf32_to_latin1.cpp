40fe7d593bd8ab9d3562f1203ac427cc38365166
Nick Nuon <myjpntrip@gmail.com>
2023-10-04 21:48:28 -0400

    Another attempt

--- src/haswell/avx2_convert_utf32_to_latin1.cpp
+++ src/haswell/avx2_convert_utf32_to_latin1.cpp
@@ -64,6 +64,24 @@ avx2_convert_utf32_to_latin1(const char32_t *buf, size_t len,
   return std::make_pair(buf, latin1_output);
 } */
 
+
+/* convert_utf32_to_latin1+haswell, input size: 1729220, iterations: 30000, dataset: /home/leorio/unicode_lipsum/wikipedia_mars/french.utflatin32.txt
+   0.250 ins/byte,    0.132 cycle/byte,   24.147 GB/s (0.9 %),     3.197 GHz,    1.890 ins/cycle 
+   1.001 ins/char,    0.530 cycle/char,    6.037 Gc/s (0.9 %)     4.00 byte/char 
+convert_utf32_to_latin1+icelake, input size: 1729220, iterations: 30000, dataset: /home/leorio/unicode_lipsum/wikipedia_mars/french.utflatin32.txt
+   0.172 ins/byte,    0.127 cycle/byte,   24.334 GB/s (0.9 %),     3.097 GHz,    1.352 ins/cycle 
+   0.688 ins/char,    0.509 cycle/char,    6.083 Gc/s (0.9 %)     4.00 byte/char 
+convert_utf32_to_latin1+iconv, input size: 1729220, iterations: 30000, dataset: /home/leorio/unicode_lipsum/wikipedia_mars/french.utflatin32.txt
+  10.006 ins/byte,    1.773 cycle/byte,    1.796 GB/s (22.2 %),     3.185 GHz,    5.643 ins/cycle 
+  40.023 ins/char,    7.093 cycle/char,    0.449 Gc/s (22.2 %)     4.00 byte/char 
+WARNING: Measurements are noisy, try increasing iteration count (-I).
+convert_utf32_to_latin1+icu, input size: 1729220, iterations: 30000, dataset: /home/leorio/unicode_lipsum/wikipedia_mars/french.utflatin32.txt
+  19.687 ins/byte,    3.691 cycle/byte,    0.864 GB/s (3.7 %),     3.189 GHz,    5.334 ins/cycle 
+  78.748 ins/char,   14.765 cycle/char,    0.216 Gc/s (3.7 %)     4.00 byte/char 
+convert_utf32_to_latin1+westmere, input size: 1729220, iterations: 30000, dataset: /home/leorio/unicode_lipsum/wikipedia_mars/french.utflatin32.txt
+   0.406 ins/byte,    0.138 cycle/byte,   23.186 GB/s (1.0 %),     3.196 GHz,    2.948 ins/cycle 
+   1.626 ins/char,    0.551 cycle/char,    5.796 Gc/s (1.0 %)     4.00 byte/char  */
+
 std::pair<const char32_t *, char *>
 avx2_convert_utf32_to_latin1(const char32_t *buf, size_t len,
                              char *latin1_output) {
@@ -71,10 +89,7 @@ avx2_convert_utf32_to_latin1(const char32_t *buf, size_t len,
       len & ~0x1F; // Round down to nearest multiple of 32
 
   __m256i high_bytes_mask = _mm256_set1_epi32(0xFFFFFF00);
-/*   __m256i shufmask = _mm256_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-                                     -1, 12, 8, 4, 0, 12, 8, 4, 0,-1, -1, -1, -1, -1, -1,
-                                     -1, -1, -1, -1, -1, -1 );
- */
+
   __m256i shufmask = _mm256_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
                                     -1, 12, 8, 4, 0, -1, -1, -1, -1, -1, -1,
                                     -1, -1, -1, -1, -1, -1, 12, 8, 4, 0);
@@ -89,35 +104,19 @@ avx2_convert_utf32_to_latin1(const char32_t *buf, size_t len,
       return std::make_pair(nullptr, latin1_output);
     }
 
-/*     __m256i shuffled1 = _mm256_shuffle_epi8(in1, shufmask);
-    _mm_storeu_si128((__m128i *)latin1_output,
-                     _mm256_castsi256_si128(shuffled1));
-    _mm_storeu_si128((__m128i *)(latin1_output + 4),
-                     _mm256_extracti128_si256(shuffled1, 1));
-
-    __m256i shuffled2 = _mm256_shuffle_epi8(in2, shufmask);
-    _mm_storeu_si128((__m128i *)(latin1_output + 8),
-                     _mm256_castsi256_si128(shuffled2));
-    *reinterpret_cast<uint32_t *>(latin1_output + 12) =
-        _mm_cvtsi128_si32(_mm256_extracti128_si256(shuffled2, 1)); */
-
     //Turn UTF32 bytes into latin 1 bytes
     __m256i shuffled1 = _mm256_shuffle_epi8(in1, shufmask);
     __m256i shuffled2 = _mm256_shuffle_epi8(in2, shufmask);
 
-    //reshuffle UTF32 bytes to their correct spot
-    __m256i idx1 = _mm256_set_epi32(0, 4,0,0,0,0,0,0); //this is likely wrong
-    __m256i idx2 = _mm256_set_epi32(0, 0,0,4,0,0,0,0);
-
+    //move UTF32 bytes to their correct spot
+    __m256i idx1 = _mm256_set_epi32(-1, -1,-1,-1,-1,-1,4,0);
+    __m256i idx2 = _mm256_set_epi32(-1, -1,-1,-1,4,0,-1,-1);
     __m256i reshuffled1 = _mm256_permutevar8x32_epi32(shuffled1, idx1);
     __m256i reshuffled2 = _mm256_permutevar8x32_epi32(shuffled2, idx2);
 
-    __m256i result = _mm256_or_si256(reshuffled2, reshuffled2);
+    __m256i result = _mm256_or_si256(reshuffled1, reshuffled2);
     _mm_storeu_si128((__m128i *)latin1_output,
                      _mm256_castsi256_si128(result));
-
-/*     _mm256_storeu_si256((__m256i*)latin1_output, interleave1);
-    _mm256_storeu_si256((__m256i*)(latin1_output + 16), interleave2); */
 
     latin1_output += 16;
     buf += 16;
