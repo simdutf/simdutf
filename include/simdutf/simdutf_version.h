69e2542fbaac7abeb0347b48798ab3f0c492d92e
Daniel Lemire <daniel@lemire.me>
2023-10-08 13:48:09 -0400

    Version 3.2.18

--- include/simdutf/simdutf_version.h
+++ include/simdutf/simdutf_version.h
@@ -4,7 +4,7 @@
 #define SIMDUTF_SIMDUTF_VERSION_H
 
 /** The version of simdutf being used (major.minor.revision) */
-#define SIMDUTF_VERSION "3.2.17"
+#define SIMDUTF_VERSION "3.2.18"
 
 namespace simdutf {
 enum {
@@ -19,7 +19,7 @@ enum {
   /**
    * The revision (major.minor.REVISION) of simdutf being used.
    */
-  SIMDUTF_VERSION_REVISION = 17
+  SIMDUTF_VERSION_REVISION = 18
 };
 } // namespace simdutf
 
