a9385cd67bf60364632086cb521002510f76f918
Daniel Lemire <dlemire@lemire.me>
2024-03-16 13:38:00 -0400

    adding missing files

---
+++ benchmarks/base64/benchmark_base64.cpp
@@ -0,0 +1,396 @@
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
+
+#include "libbase64.h"
+#include "simdutf.h"
+#include "node_base64.h"
+
+#include "event_counter.h"
+#include <atomic>
+
+bool is_space(char c) {
+  static const bool is_space[256] = {
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  return is_space[uint8_t(c)];
+}
+// This is for reference only, do not use this function in production
+// system.
+int base64_decode_skip_spaces(const char *src, size_t srclen, char *out,
+                                     size_t *outlen) {
+  struct base64_state state;
+  base64_stream_decode_init(&state, 0);
+  const char *srcend = src + srclen;
+  char *initout = out;
+  // If the input starts with spaces, we skip them.
+  while (src < srcend && is_space(*src)) {
+    src++;
+  }
+  while (src < srcend) {
+    size_t len = 0;
+    const char *srcendtoken = src;
+    while (srcendtoken < srcend && !is_space(*srcendtoken)) {
+      srcendtoken++;
+    }
+    int r = base64_stream_decode(&state, src, size_t(srcendtoken - src),
+                                 (char *)out, &len);
+    if (r != 1) {
+      return -1; // fatal error.
+    }
+    out += len;
+    src = srcendtoken;
+    while (src < srcend && is_space(*src)) {
+      src++;
+    }
+  }
+
+  *outlen = size_t(out - initout);
+  return !state.bytes;
+}
+
+enum : uint8_t { roundtrip = 0, decode = 1, encode = 2 };
+
+event_collector collector;
+
+template <class function_type>
+event_aggregate bench(const function_type &function, size_t min_repeat = 10,
+                      size_t min_time_ns = 1000000000,
+                      size_t max_repeat = 1000000) {
+  event_aggregate aggregate{};
+  size_t N = min_repeat;
+  if (N == 0) {
+    N = 1;
+  }
+  for (size_t i = 0; i < N; i++) {
+    std::atomic_thread_fence(std::memory_order_acquire);
+    collector.start();
+    function();
+    std::atomic_thread_fence(std::memory_order_release);
+    event_count allocate_count = collector.end();
+    aggregate << allocate_count;
+    if ((i + 1 == N) && (aggregate.total_elapsed_ns() < min_time_ns) &&
+        (N < max_repeat)) {
+      N *= 10;
+    }
+  }
+  return aggregate;
+}
+
+std::vector<char> read_file(const char *filename,
+                            bool trim_final_newline = true) {
+  std::ifstream file;
+  file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  file.open(filename);
+  std::vector<char> input_data;
+  input_data.assign(std::istreambuf_iterator<char>(file),
+                    std::istreambuf_iterator<char>());
+  if (trim_final_newline && !input_data.empty() && input_data.back() == '\n') {
+    input_data.pop_back();
+  }
+  return input_data;
+}
+
+void show_help() {
+  printf("Usage: benchmark_base64 [options] file1 [file2 ...]\n");
+  printf("Options:\n");
+  printf("  -h, --help     Show this help message and exit\n");
+  printf("  -d, --decode   Decode the input file\n");
+  printf("  -e, --encode   Encode the input file\n");
+  printf("  -r, --roundtrip   Roundtrip the input file\n");
+  printf(" See https://github.com/lemire/base64data for test data.\n");
+}
+void pretty_print(size_t, size_t bytes, std::string name, event_aggregate agg) {
+  printf("%-40s : ", name.c_str());
+  printf(" %5.2f GB/s ", bytes / agg.elapsed_ns());
+  if (collector.has_events()) {
+    printf(" %5.2f GHz ", agg.cycles() / agg.elapsed_ns());
+    printf(" %5.2f c/b ", agg.cycles() / bytes);
+    printf(" %5.2f i/b ", agg.instructions() / bytes);
+    printf(" %5.2f i/c ", agg.instructions() / agg.cycles());
+  }
+  printf("\n");
+}
+bool can_decode_all(std::vector<std::vector<char>> &data) {
+  for (const std::vector<char> &source : data) {
+    std::vector<char> buffer(simdutf::maximal_binary_length_from_base64(
+        source.data(), source.size()));
+
+    auto err =
+        simdutf::base64_to_binary(source.data(), source.size(), buffer.data());
+    if (err.error) {
+      return false;
+    }
+  }
+  return true;
+}
+
+std::vector<std::vector<char>> split(const std::vector<char> &data) {
+  std::vector<std::vector<char>> answer;
+  const char *p = data.data();
+  const char *end = p + data.size();
+  while (p < end) {
+    const char *q = p;
+    while ((q < end) && (*q != '\n') && (*q != '\r')) {
+      q++;
+    }
+    answer.emplace_back(p, q);
+    p = q;
+    if (p < end) {
+      p++;
+    }
+    while (p < end && ((*p == '\n') || (*p == '\r'))) {
+      p++;
+    }
+  }
+  return answer;
+}
+
+void check_for_single_line(std::vector<std::vector<char>> &data) {
+  if (can_decode_all(data)) {
+    return;
+  }
+  printf("# I cannot decode all the base64 data as single line, so I will try "
+         "to split the data.\n");
+  std::vector<std::vector<char>> newdata;
+  for (const std::vector<char> &source : data) {
+    auto splits = split(source);
+    for (auto &split : splits) {
+      newdata.push_back(split);
+    }
+  }
+  printf("# Turned %zu inputs into %zu inputs.\n", data.size(), newdata.size());
+
+  data = std::move(newdata);
+}
+
+bool contains_spaces(std::vector<std::vector<char>> &data) {
+  for (const std::vector<char> &source : data) {
+    for (char c : source) {
+      if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void bench(std::vector<std::vector<char>> &data, uint8_t mode) {
+  size_t volume = std::accumulate(
+      data.begin(), data.end(), 0,
+      [](size_t a, const std::vector<char> &b) { return a + b.size(); });
+  size_t max_size = std::max_element(data.begin(), data.end(),
+                                     [](const std::vector<char> &a,
+                                        const std::vector<char> &b) {
+                                       return a.size() < b.size();
+                                     })
+                        ->size();
+  std::vector<char> buffer1(simdutf::base64_length_from_binary(max_size));
+  std::vector<char> buffer2(max_size);
+  printf("# volume: %zu bytes\n", volume);
+  printf("# max length: %zu bytes\n", max_size);
+  printf("# number of inputs: %zu\n", data.size());
+
+  switch (mode) {
+  case roundtrip: {
+    printf("# roundtrip\n");
+    pretty_print(
+        data.size(), volume, "libbase64", bench([&data, &buffer1, &buffer2]() {
+          for (const std::vector<char> &source : data) {
+
+            size_t outlen;
+
+            base64_encode(source.data(), source.size(), buffer1.data(), &outlen,
+                          0);
+            int result = base64_decode(buffer1.data(), outlen, buffer2.data(),
+                                       &outlen, 0);
+            if (result != 1) {
+              std::cerr << "Error: " << result << " failed to decode base64 "
+                        << std::endl;
+            }
+            if (outlen != source.size()) {
+              std::cerr << "Error: " << result << " bytes decoded, expected "
+                        << source.size() << std::endl;
+            }
+          }
+        }));
+    for (auto &e : simdutf::get_available_implementations()) {
+      if (!e->supported_by_runtime_system()) {
+        continue;
+      }
+      pretty_print(data.size(), volume, "simdutf::" + e->name(),
+                   bench([&data, &buffer1, &buffer2, &e]() {
+                     for (const std::vector<char> &source : data) {
+                       size_t base64_size = e->binary_to_base64(
+                           source.data(), source.size(), buffer1.data());
+                       auto err = e->base64_to_binary(
+                           buffer1.data(), base64_size, buffer2.data());
+                       if (err.error) {
+                         std::cerr << "Error:  at position " << err.count
+                                   << std::endl;
+                       } else if (err.count != source.size()) {
+                         std::cerr << "Error: " << err.count
+                                   << " bytes decoded, expected "
+                                   << source.size() << std::endl;
+                       }
+                     }
+                   }));
+    }
+    break;
+  }
+  case decode: {
+    printf("# decode\n");
+    bool spaces = contains_spaces(data);
+    if (spaces) {
+      printf("# the base64 data contains spaces, so we cannot use straigth "
+             "libbase64::base64_decode\n");
+    } else {
+      pretty_print(data.size(), volume, "libbase64",
+                   bench([&data, &buffer1, &buffer2]() {
+                     for (const std::vector<char> &source : data) {
+
+                       size_t outlen;
+                       int result = base64_decode(source.data(), source.size(),
+                                                  buffer1.data(), &outlen, 0);
+                       if (result != 1) {
+                         std::cerr << "Error: " << result
+                                   << " failed to decode base64 " << std::endl;
+                         throw std::runtime_error(
+                             "Error: failed to decode base64 ");
+                       }
+                     }
+                   }));
+    }
+    pretty_print(data.size(), volume, "node",
+                 bench([&data, &buffer1, &buffer2]() {
+                   for (const std::vector<char> &source : data) {
+                     int result = node::base64_decode(buffer1.data(), buffer1.size(),
+                                    source.data(), source.size());
+                     (void) result;
+                   }
+                 }));
+    for (auto &e : simdutf::get_available_implementations()) {
+      if (!e->supported_by_runtime_system()) {
+        continue;
+      }
+      pretty_print(
+          data.size(), volume, "simdutf::" + e->name(),
+          bench([&data, &buffer1, &buffer2, &e]() {
+            for (const std::vector<char> &source : data) {
+              auto err = e->base64_to_binary(source.data(), source.size(),
+                                             buffer1.data());
+              if (err.error) {
+                std::cerr << "Error: at position " << err.count << " out of "
+                          << source.size() << std::endl;
+                for (size_t i = err.count; i < source.size(); i++) {
+                  printf("0x%02x (%c) ", uint8_t(source[i]), source[i]);
+                }
+                printf("\n");
+                throw std::runtime_error("Error: is input valid base64? " +
+                                         std::to_string(err.error) +
+                                         " at position " +
+                                         std::to_string(err.count));
+              }
+            }
+          }));
+    }
+    break;
+  }
+  case encode: {
+    printf("# encode\n");
+    volatile size_t base64_size;
+    pretty_print(data.size(), volume, "libbase64",
+                 bench([&data, &buffer1, &buffer2, &base64_size]() {
+                   for (const std::vector<char> &source : data) {
+                     size_t outlen;
+                     base64_encode(source.data(), source.size(), buffer1.data(),
+                                   &outlen, 0);
+                     base64_size = outlen;
+                   }
+                 }));
+    for (auto &e : simdutf::get_available_implementations()) {
+      if (!e->supported_by_runtime_system()) {
+        continue;
+      }
+      pretty_print(data.size(), volume, "simdutf::" + e->name(),
+                   bench([&data, &buffer1, &buffer2, &e, &base64_size]() {
+                     for (const std::vector<char> &source : data) {
+                       base64_size = e->binary_to_base64(
+                           source.data(), source.size(), buffer1.data());
+                     }
+                   }));
+    }
+    break;
+  }
+  }
+}
+
+int main(int argc, char **argv) {
+  printf("# current system detected as %s.\n",
+         simdutf::get_active_implementation()->name().c_str());
+  if (argc < 2) {
+    show_help();
+    return EXIT_FAILURE;
+  }
+
+  std::vector<std::string> arguments;
+  auto mode = roundtrip;
+
+  for (int i = 1; i < argc; i++) {
+    std::string arg{argv[i]};
+    if ((arg == "-h") || (arg == "--help")) {
+      show_help();
+      return EXIT_SUCCESS;
+    } else if ((arg == "-d") || (arg == "--decode")) {
+      mode = decode;
+    } else if ((arg == "-e") || (arg == "--encode")) {
+      mode = encode;
+    } else if ((arg == "-r") || (arg == "--roundtrip")) {
+      mode = roundtrip;
+    } else {
+      arguments.push_back(std::move(arg));
+    }
+  }
+  auto return_value = EXIT_SUCCESS;
+  std::vector<std::vector<char>> input;
+  printf("# loading files: ");
+  for (auto &arg : arguments) {
+    try {
+      printf("."); // show progress
+      fflush(stdout);
+      input.push_back(read_file(arg.c_str(), mode == decode));
+    } catch (const std::exception &e) {
+      printf("\n# tried loading file: %s\n", arg.c_str());
+      std::cerr << "Error: " << e.what() << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+  printf("\n");
+  if (mode == decode) {
+    check_for_single_line(input);
+  }
+  try {
+    bench(input, mode);
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return_value = EXIT_FAILURE;
+  }
+
+  return return_value;
+}
---
+++ benchmarks/base64/node_base64.h
@@ -0,0 +1,162 @@
+// Taken from Node.js, for benchmarking purposes.
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+namespace node {
+//// Base 64 ////
+
+enum class Base64Mode { NORMAL, URL };
+
+// Doesn't check for padding at the end.  Can be 1-2 bytes over.
+inline constexpr size_t base64_decoded_size_fast(size_t size) {
+  // 1-byte input cannot be decoded
+  return size > 1 ? (size / 4) * 3 + (size % 4 + 1) / 2 : 0;
+}
+
+inline uint32_t ReadUint32BE(const unsigned char *p);
+
+template <typename TypeName>
+size_t base64_decoded_size(const TypeName *src, size_t size);
+
+template <typename TypeName>
+size_t base64_decode(char *const dst, const size_t dstlen,
+                     const TypeName *const src, const size_t srclen);
+
+inline size_t base64_encode(const char *src, size_t slen, char *dst,
+                            size_t dlen, Base64Mode mode = Base64Mode::NORMAL);
+} // namespace node
+
+namespace node {
+/*
+static constexpr char base64_table_url[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                           "abcdefghijklmnopqrstuvwxyz"
+                                           "0123456789-_";*/
+
+/*extern const int8_t unbase64_table[256];*/
+
+// supports regular and URL-safe base64
+const int8_t unbase64_table[256] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, -1, -1, -2, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, 62, -1, 62, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, -1, -1, -1, -1, -1, -1, -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1,
+    63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+    43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+inline int8_t unbase64(uint8_t x) { return unbase64_table[x]; }
+
+inline uint32_t ReadUint32BE(const unsigned char *p) {
+  return static_cast<uint32_t>(p[0] << 24U) |
+         static_cast<uint32_t>(p[1] << 16U) |
+         static_cast<uint32_t>(p[2] << 8U) | static_cast<uint32_t>(p[3]);
+}
+
+#ifdef _MSC_VER
+#pragma warning(push)
+// MSVC C4003: not enough actual parameters for macro 'identifier'
+#pragma warning(disable : 4003)
+#endif
+template <typename TypeName>
+bool base64_decode_group_slow(char *const dst, const size_t dstlen,
+                              const TypeName *const src, const size_t srclen,
+                              size_t *const i, size_t *const k) {
+  uint8_t hi;
+  uint8_t lo;
+#define V(expr)                                                                \
+  for (;;) {                                                                   \
+    const uint8_t c = static_cast<uint8_t>(src[*i]);                           \
+    lo = uint8_t(unbase64(c));                                                 \
+    *i += 1;                                                                   \
+    if (lo < 64)                                                               \
+      break; /* Legal character. */                                            \
+    if (c == '=' || *i >= srclen)                                              \
+      return false; /* Stop decoding. */                                       \
+  }                                                                            \
+  expr;                                                                        \
+  if (*i >= srclen)                                                            \
+    return false;                                                              \
+  if (*k >= dstlen)                                                            \
+    return false;                                                              \
+  hi = lo;
+  V(/* Nothing. */);
+  V(dst[(*k)++] = char((hi & 0x3F) << 2) | ((lo & 0x30) >> 4));
+  V(dst[(*k)++] = char((hi & 0x0F) << 4) | ((lo & 0x3C) >> 2));
+  V(dst[(*k)++] = char((hi & 0x03) << 6) | ((lo & 0x3F) >> 0));
+#undef V
+  return true; // Continue decoding.
+}
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+template <typename TypeName>
+size_t base64_decode_fast(char *const dst, const size_t dstlen,
+                          const TypeName *const src, const size_t srclen,
+                          const size_t decoded_size) {
+  const size_t available = dstlen < decoded_size ? dstlen : decoded_size;
+  const size_t max_k = available / 3 * 3;
+  size_t max_i = srclen / 4 * 4;
+  size_t i = 0;
+  size_t k = 0;
+  while (i < max_i && k < max_k) {
+    const unsigned char txt[] = {
+        static_cast<unsigned char>(unbase64(static_cast<uint8_t>(src[i + 0]))),
+        static_cast<unsigned char>(unbase64(static_cast<uint8_t>(src[i + 1]))),
+        static_cast<unsigned char>(unbase64(static_cast<uint8_t>(src[i + 2]))),
+        static_cast<unsigned char>(unbase64(static_cast<uint8_t>(src[i + 3]))),
+    };
+
+    const uint32_t v = ReadUint32BE(txt);
+    // If MSB is set, input contains whitespace or is not valid base64.
+    if (v & 0x80808080) {
+      if (!base64_decode_group_slow(dst, dstlen, src, srclen, &i, &k))
+        return k;
+      max_i = i + (srclen - i) / 4 * 4; // Align max_i again.
+    } else {
+      dst[k + 0] = char(((v >> 22) & 0xFC) | ((v >> 20) & 0x03));
+      dst[k + 1] = char(((v >> 12) & 0xF0) | ((v >> 10) & 0x0F));
+      dst[k + 2] = char(((v >> 2) & 0xC0) | ((v >> 0) & 0x3F));
+      i += 4;
+      k += 3;
+    }
+  }
+  if (i < srclen && k < dstlen) {
+    base64_decode_group_slow(dst, dstlen, src, srclen, &i, &k);
+  }
+  return k;
+}
+
+template <typename TypeName>
+size_t base64_decoded_size(const TypeName *src, size_t size) {
+  // 1-byte input cannot be decoded
+  if (size < 2)
+    return 0;
+
+  if (src[size - 1] == '=') {
+    size--;
+    if (src[size - 1] == '=')
+      size--;
+  }
+  return base64_decoded_size_fast(size);
+}
+
+template <typename TypeName>
+size_t base64_decode(char *const dst, const size_t dstlen,
+                     const TypeName *const src, const size_t srclen) {
+  const size_t decoded_size = base64_decoded_size(src, srclen);
+  return base64_decode_fast(dst, dstlen, src, srclen, decoded_size);
+}
+
+template size_t base64_decode<char>(char *const dst, const size_t dstlen,
+                                    const char *const src, const size_t srclen);
+
+} // namespace node
