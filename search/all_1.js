39c9acbc70f92d820f3bad74c1537275d4113bf8
lemire <lemire@users.noreply.github.com>
2024-04-01 15:34:48 +0000

    deploy: 420b1613566c420efaea80882e1182735c71df04

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['base64_5flength_5ffrom_5fbinary_1',['base64_length_from_binary',['../classsimdutf_1_1implementation.html#a74dd85eb6ea2ef6aa76faa537c2ac49e',1,'simdutf::implementation']]],
-  ['base64_5fto_5fbinary_2',['base64_to_binary',['../classsimdutf_1_1implementation.html#ac04f8fb32ad66cc4e5c0d13d4f9461ea',1,'simdutf::implementation']]],
-  ['binary_5fto_5fbase64_3',['binary_to_base64',['../classsimdutf_1_1implementation.html#acf529607eb82bef95b548289af5d36ef',1,'simdutf::implementation']]]
+  ['base64_5fto_5fbinary_2',['base64_to_binary',['../classsimdutf_1_1implementation.html#a73d3db16d1520bb7ee958692f8ac6bc9',1,'simdutf::implementation::base64_to_binary(const char *input, size_t length, char *output, base64_options options=base64_default) const noexcept=0'],['../classsimdutf_1_1implementation.html#ab209f124bfae37db124194317450f88a',1,'simdutf::implementation::base64_to_binary(const char16_t *input, size_t length, char *output, base64_options options=base64_default) const noexcept=0']]],
+  ['binary_5fto_5fbase64_3',['binary_to_base64',['../classsimdutf_1_1implementation.html#a5823f6a86315f4404a4f437977abf9df',1,'simdutf::implementation']]]
 ];
