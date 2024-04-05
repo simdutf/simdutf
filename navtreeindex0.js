afcc1590efaada3515548c0ff54824bec2306349
lemire <lemire@users.noreply.github.com>
2024-04-05 19:42:40 +0000

    deploy: df3fd1a23580b8a521d3af6ad35765575a897f3b

--- navtreeindex0.js
+++ navtreeindex0.js
@@ -126,6 +126,6 @@ var NAVTREEINDEX0 =
 "structsimdutf_1_1result.html":[2,0,0,0],
 "structsimdutf_1_1result.html#a22e8e209bc2dc6bed08c1d7856dc0c85":[2,0,0,0,2],
 "structsimdutf_1_1result.html#a33e728384a994b1cbfa2bde2192a718c":[2,0,0,0,3],
-"structsimdutf_1_1result.html#a48105ee3dd3686a525cd46b7ce804f76":[2,0,0,0,1],
+"structsimdutf_1_1result.html#a7561b79537154585f0d03547abc5ad4f":[2,0,0,0,1],
 "structsimdutf_1_1result.html#ac4e7a7f4d0f7b4f61a79508690cc3fe0":[2,0,0,0,0]
 };
--- structsimdutf_1_1result.js
+++ structsimdutf_1_1result.js
@@ -1,7 +1,7 @@
 var structsimdutf_1_1result =
 [
     [ "result", "structsimdutf_1_1result.html#ac4e7a7f4d0f7b4f61a79508690cc3fe0", null ],
-    [ "result", "structsimdutf_1_1result.html#a48105ee3dd3686a525cd46b7ce804f76", null ],
+    [ "result", "structsimdutf_1_1result.html#a7561b79537154585f0d03547abc5ad4f", null ],
     [ "count", "structsimdutf_1_1result.html#a22e8e209bc2dc6bed08c1d7856dc0c85", null ],
     [ "error", "structsimdutf_1_1result.html#a33e728384a994b1cbfa2bde2192a718c", null ]
 ];
