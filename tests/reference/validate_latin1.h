a3d7f3119ba393a379ab452a094426a0f18e5250
Daniel Lemire <daniel@lemire.me>
2024-04-29 11:32:47 -0400

    fix: removing dead code

--- tests/reference/validate_latin1.h
+++
@@ -1,8 +0,0 @@
-#include "simdutf/common_defs.h"
-
-
-namespace simdutf {  namespace tests { namespace reference {
-
-    simdutf_warn_unused bool validate_latin1(const char *buf, size_t len) noexcept;
-
-}}}
