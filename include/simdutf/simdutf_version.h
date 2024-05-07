2044c6e94637fc6dd2f73726f56637a834990675
Daniel Lemire <daniel@lemire.me>
2024-05-07 19:08:08 -0400

    version bump

--- include/simdutf/simdutf_version.h
+++ include/simdutf/simdutf_version.h
@@ -4,7 +4,7 @@
 #define SIMDUTF_SIMDUTF_VERSION_H
 
 /** The version of simdutf being used (major.minor.revision) */
-#define SIMDUTF_VERSION "5.2.6"
+#define SIMDUTF_VERSION "5.2.7"
 
 namespace simdutf {
 enum {
@@ -19,7 +19,7 @@ enum {
   /**
    * The revision (major.minor.REVISION) of simdutf being used.
    */
-  SIMDUTF_VERSION_REVISION = 6
+  SIMDUTF_VERSION_REVISION = 7
 };
 } // namespace simdutf
 
