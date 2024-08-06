592911195343aef2e90461586f88adc1f5e9b76d
Daniel Lemire <daniel@lemire.me>
2024-08-05 21:55:22 -0400

    verify issue 479 (#482)

--- tests/convert_utf8_to_utf16be_with_errors_tests.cpp
+++ tests/convert_utf8_to_utf16be_with_errors_tests.cpp
@@ -18,6 +18,34 @@ namespace {
   constexpr size_t fix_size = 512;
 }
 
+TEST(issue_479) {
+    const unsigned char data[] = {0xf1, 0xa1, 0xa9, 0xa9, 0xf1, 0xa1, 0xa9, 0xa9, 0xf1, 0xa1, 0xa9,
+                                  0xb2, 0xf1, 0xb9, 0xa1, 0xa9, 0xf1, 0xa1, 0xa9, 0xa9, 0xf1, 0xa1,
+                                  0xa9, 0xa9, 0xf1, 0xa9, 0xa1, 0xa9, 0xf1, 0xa1, 0xae, 0xa6, 0xf1,
+                                  0xa1, 0xa9, 0xa9, 0xf1, 0xa9, 0xa1, 0xa9, 0xf1, 0xa1, 0xa9, 0xa9,
+                                  0xf1, 0xa1, 0xa9, 0xa9, 0xf1, 0xa1, 0xa9, 0xa9, 0xf1, 0xa1, 0xa9,
+                                  0xa9, 0xf1, 0xa1, 0xa9, 0xa9, 0xf1, 0xa0, 0xa0, 0xa0, 0xa0};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char);
+    const auto validation1 = implementation.validate_utf8_with_errors((const char *) data, data_len);
+    ASSERT_EQUAL(validation1.count, 64);
+    ASSERT_EQUAL(validation1.error, simdutf::error_code::TOO_LONG);
+
+    const auto outlen = implementation.utf16_length_from_utf8((const char *) data, data_len);
+    ASSERT_EQUAL(outlen, 32);
+    std::vector<char16_t> output(outlen);
+    const auto r = implementation.convert_utf8_to_utf16be_with_errors((const char *) data,
+                                                                      data_len,
+                                                                      output.data());
+    ASSERT_EQUAL(r.error, simdutf::error_code::SUCCESS);
+    ASSERT_EQUAL(r.count, 1234);
+    const std::vector<char16_t> expected_out{};
+    ASSERT_TRUE(output.size() == expected_out.size());
+    for (std::size_t i = 0; i < output.size(); ++i) {
+        ASSERT_EQUAL(+output.at(i), +expected_out.at(i));
+    };
+}
+
 
 TEST(issue_convert_utf8_to_utf16be_with_errors_a8ec246845d4878e) {
     const unsigned char data[] = {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
