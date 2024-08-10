b7b34c14cf170c63ef49f15d2297e1775638eb4c
Paul Dreik <github@pauldreik.se>
2024-08-10 21:47:28 +0200

    remove duplicate line

--- include/simdutf/implementation.h
+++ include/simdutf/implementation.h
@@ -1409,7 +1409,6 @@ simdutf_warn_unused size_t trim_partial_utf16(const char16_t* input, size_t leng
 
 // base64_options are used to specify the base64 encoding options.
 using base64_options = uint64_t;
-using base64_options = uint64_t;
 enum : base64_options {
   base64_default = 0, /* standard base64 format (with padding) */
   base64_url = 1, /* base64url format (no padding) */
