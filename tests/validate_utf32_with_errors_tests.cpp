e31092cb873a6245b6874264dab04b993b3a4883
Daniel Lemire <daniel@lemire.me>
2024-08-18 17:35:51 -0400

    fixing issue 531 (#534)
    Co-authored-by: Daniel Lemire <dlemire@lemire.me>

--- tests/validate_utf32_with_errors_tests.cpp
+++ tests/validate_utf32_with_errors_tests.cpp
@@ -3,6 +3,20 @@
 #include <tests/helpers/random_utf32.h>
 #include <tests/helpers/test.h>
 
+TEST(issue_531)
+{
+    const char32_t data[] = {0xdbdb, 0xff380000, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 
+                            0x00ff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 
+                            0x0000, 0x0000, 0x0000, 0x0000, 0x0000};
+    constexpr std::size_t data_len = sizeof(data);
+    const auto validation1 = implementation.validate_utf32_with_errors((const char32_t *) data,
+                                                                       data_len);
+    // got return [count=1, error=TOO_LARGE] from implementation rvv
+    // got return [count=0, error=SURROGATE] from implementation fallback
+    ASSERT_EQUAL(validation1.error, simdutf::error_code::SURROGATE);
+    ASSERT_EQUAL(validation1.count, 0);
+}
+
 TEST_LOOP(1000, validate_utf32_with_errors__returns_success_for_valid_input) {
    simdutf::tests::helpers::random_utf32 generator{seed};
     const auto utf32{generator.generate(256, seed)};
