5437c4532724d0cf1185cbb30cf7fe635c0a7cb9
Daniel Lemire <dlemire@lemire.me>
2024-12-17 00:17:27 -0500

    tweaking optimization

--- src/scalar/base64.h
+++ src/scalar/base64.h
@@ -81,23 +81,24 @@ base64_tail_decode(char *dst, const char_type *src, size_t length,
     idx = 0;
     // we need at least four characters.
     // If possible, we read four characters at a time. (It is an optimization.)
-    if(src + 4 <= srcend) {
-      // We expect the next loop to get unrolled by the compiler.
-      for(int k = 0; k < 4; k++) {
-        char_type c = *src;
-        uint8_t code = to_base64[uint8_t(c)];
-        buffer[idx] = code;
-        if (is_eight_byte(c) && code <= 63) {
-          idx++;
-        } else if (!ignore_garbage &&
-                  (code > 64 || !scalar::base64::is_eight_byte(c))) {
-          return {INVALID_BASE64_CHARACTER, size_t(src - srcinit),
-                  size_t(dst - dstinit)};
-        } else {
-          // We have a space or a newline or garbage. We ignore it.
-        }
-        src++;
-      }
+    if(ignore_garbage && src + 4 <= srcend) {
+        char_type c0 = src[0];
+        char_type c1 = src[1];
+        char_type c2 = src[2];
+        char_type c3 = src[3];
+        uint8_t code0 = to_base64[uint8_t(c0)];
+        uint8_t code1 = to_base64[uint8_t(c1)];
+        uint8_t code2 = to_base64[uint8_t(c2)];
+        uint8_t code3 = to_base64[uint8_t(c3)];
+        buffer[idx] = code0;
+        idx += (is_eight_byte(c0) && code0 <= 63);
+        buffer[idx] = code1;
+        idx += (is_eight_byte(c1) && code1 <= 63);
+        buffer[idx] = code2;
+        idx += (is_eight_byte(c2) && code2 <= 63);
+        buffer[idx] = code3;
+        idx += (is_eight_byte(c3) && code3 <= 63);
+	src += 4;
     }
     while ((idx < 4) & (src < srcend)) {
       char_type c = *src;
@@ -254,23 +255,24 @@ result base64_tail_decode_safe(
     const char_type *srccur = src;
     // We need at least four characters.
     // If possible, we read four characters at a time. (It is an optimization.)
-    if(src + 4 <= srcend) {
-      // We expect the next loop to get unrolled by the compiler.
-      for(int k = 0; k < 4; k++) {
-        char_type c = *src;
-        uint8_t code = to_base64[uint8_t(c)];
-        buffer[idx] = code;
-        if (is_eight_byte(c) && code <= 63) {
-          idx++;
-        } else if (!ignore_garbage &&
-                  (code > 64 || !scalar::base64::is_eight_byte(c))) {
-          return {INVALID_BASE64_CHARACTER, size_t(src - srcinit),
-                  size_t(dst - dstinit)};
-        } else {
-          // We have a space or a newline or garbage. We ignore it.
-        }
-        src++;
-      }
+    if(ignore_garbage && src + 4 <= srcend) {
+        char_type c0 = src[0];
+        char_type c1 = src[1];
+        char_type c2 = src[2];
+        char_type c3 = src[3];
+        uint8_t code0 = to_base64[uint8_t(c0)];
+        uint8_t code1 = to_base64[uint8_t(c1)];
+        uint8_t code2 = to_base64[uint8_t(c2)];
+        uint8_t code3 = to_base64[uint8_t(c3)];
+        buffer[idx] = code0;
+        idx += (is_eight_byte(c0) && code0 <= 63);
+        buffer[idx] = code1;
+        idx += (is_eight_byte(c1) && code1 <= 63);
+        buffer[idx] = code2;
+        idx += (is_eight_byte(c2) && code2 <= 63);
+        buffer[idx] = code3;
+        idx += (is_eight_byte(c3) && code3 <= 63);
+	src += 4;
     }
     while (idx < 4 && src < srcend) {
       char_type c = *src;
