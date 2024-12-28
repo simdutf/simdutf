65a1448cb09b3ece6f8c71bc070576487679ecaf
Shreesh Adiga <54449449+tantei3@users.noreply.github.com>
2024-12-28 19:37:22 +0530

    Optimize icelake validate_utf32 for small inputs using masked AVX512 instructions (#622)
    * Optimize icelake validate_utf32 for small inputs using masked AVX512 instructions
    
    * Adding explicit typecast to fix -Werror=conversion
    
    * Adding varying len tests and fixing mask calculation

--- src/icelake/icelake_utf32_validation.inl.cpp
+++ src/icelake/icelake_utf32_validation.inl.cpp
@@ -1,16 +1,16 @@
 // file included directly
 
-const char32_t *validate_utf32(const char32_t *buf, size_t len) {
-  if (len < 16) {
-    return buf;
+bool validate_utf32(const char32_t *buf, size_t len) {
+  if (len == 0) {
+    return true;
   }
-  const char32_t *end = buf + len - 16;
+  const char32_t *end = buf + len;
 
   const __m512i offset = _mm512_set1_epi32((uint32_t)0xffff2000);
   __m512i currentmax = _mm512_setzero_si512();
   __m512i currentoffsetmax = _mm512_setzero_si512();
 
-  while (buf <= end) {
+  while (buf < end - 16) {
     __m512i utf32 = _mm512_loadu_si512((const __m512i *)buf);
     buf += 16;
     currentoffsetmax =
@@ -18,18 +18,24 @@ const char32_t *validate_utf32(const char32_t *buf, size_t len) {
     currentmax = _mm512_max_epu32(utf32, currentmax);
   }
 
+  __m512i utf32 =
+      _mm512_maskz_loadu_epi32(__mmask16((1 << (end - buf)) - 1), buf);
+  currentoffsetmax =
+      _mm512_max_epu32(_mm512_add_epi32(utf32, offset), currentoffsetmax);
+  currentmax = _mm512_max_epu32(utf32, currentmax);
+
   const __m512i standardmax = _mm512_set1_epi32((uint32_t)0x10ffff);
   const __m512i standardoffsetmax = _mm512_set1_epi32((uint32_t)0xfffff7ff);
   __m512i is_zero =
       _mm512_xor_si512(_mm512_max_epu32(currentmax, standardmax), standardmax);
   if (_mm512_test_epi8_mask(is_zero, is_zero) != 0) {
-    return nullptr;
+    return false;
   }
   is_zero = _mm512_xor_si512(
       _mm512_max_epu32(currentoffsetmax, standardoffsetmax), standardoffsetmax);
   if (_mm512_test_epi8_mask(is_zero, is_zero) != 0) {
-    return nullptr;
+    return false;
   }
 
-  return buf;
+  return true;
 }
--- tests/validate_utf32_basic_tests.cpp
+++ tests/validate_utf32_basic_tests.cpp
@@ -9,6 +9,10 @@ TEST_LOOP(1000, validate_utf32_returns_true_for_valid_input) {
   simdutf::tests::helpers::random_utf32 generator{seed};
   const auto utf32{generator.generate(256, seed)};
 
+  for (size_t i = 0; i < utf32.size(); i++) {
+    ASSERT_TRUE(implementation.validate_utf32(
+        reinterpret_cast<const char32_t *>(utf32.data()), i + 1));
+  }
   ASSERT_TRUE(implementation.validate_utf32(
       reinterpret_cast<const char32_t *>(utf32.data()), utf32.size()));
 }
@@ -31,6 +35,7 @@ TEST_LOOP(10, validate_utf32_returns_false_when_input_in_forbidden_range) {
       utf32[i] = wrong_value;
 
       ASSERT_FALSE(implementation.validate_utf32(buf, len));
+      ASSERT_FALSE(implementation.validate_utf32(buf, i + 1));
 
       utf32[i] = old;
     }
@@ -54,6 +59,7 @@ TEST_LOOP(1000, validate_utf32_returns_false_when_input_too_large) {
       utf32[i] = wrong_value;
 
       ASSERT_FALSE(implementation.validate_utf32(buf, len));
+      ASSERT_FALSE(implementation.validate_utf32(buf, i + 1));
 
       utf32[i] = old;
     }
