a62a4acfacd6bb494cf320f12df3ba86332c2618
Paul Dreik <github@pauldreik.se>
2024-12-30 20:12:12 +0100

    add span overload for validate_utf16be_with_errors

--- include/simdutf/implementation.h
+++ include/simdutf/implementation.h
@@ -364,6 +364,14 @@ validate_utf16le_with_errors(const Span &input) noexcept {
  */
 simdutf_warn_unused result validate_utf16be_with_errors(const char16_t *buf,
                                                         size_t len) noexcept;
+#if SIMDUTF_CPLUSPLUS20
+template <typename Span>
+  requires detail::span_of_char16<Span>
+simdutf_really_inline simdutf_warn_unused result
+validate_utf16be_with_errors(const Span &input) noexcept {
+  return validate_utf16be_with_errors(input.data(), input.size());
+}
+#endif
 
 /**
  * Validate the UTF-32 string. This function may be best when you expect
