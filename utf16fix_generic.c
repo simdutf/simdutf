0f5a95503bbc2a0792a00d8689a1144baf5c7166
Robert Clausecker <fuz@fuz.su>
2025-01-07 01:22:45 +0100

    add utf16fix_generic implementation
    An accelerated generic implementation.
    53% faster than the reference implementation on my laptop.

--- utf16fix_generic.c
+++ utf16fix_generic.c
@@ -4,29 +4,49 @@
 
 #include "utf16fix.h"
 
-void
-utf16fix_generic(char16_t *out, const char16_t *in, size_t n)
+static inline void
+utf16fix_inner(char16_t *out, const char16_t *in, size_t n, bool inplace)
 {
 	size_t i;
-	bool high_surrogate_prev = false, high_surrogate, low_surrogate;
+	char16_t curr;
+	bool prev_hi = false, curr_lo;
 
-	for (i = 0; i < n; i++) {
-		high_surrogate = is_high_surrogate(in[i]);
-		low_surrogate = is_low_surrogate(in[i]);
+	if (n == 0)
+		return;
 
-		if (high_surrogate_prev && !low_surrogate)
-			out[i - 1] = 0xfffd;
+	curr = in[0] & 0xfc00;
+	out[0] = curr == 0xdc00 ? 0xfffd : in[0];
+	prev_hi = curr == 0xd800;
 
-		if (!high_surrogate_prev && low_surrogate)
-			out[i] = 0xfffd;
-		else
+	for (i = 1; i < n; i++) {
+		curr = in[i] & 0xfc00;
+		curr_lo = curr == 0xdc00;
+
+		/* mismatch? */
+		if (prev_hi ^ curr_lo) {
+			bool prev_ill, curr_ill;
+
+			prev_ill = prev_hi & !curr_lo;
+			curr_ill = !prev_hi & curr_lo;
+
+			out[i - 1] = prev_ill ? 0xfffd : out[i - 1];
+			out[i] = curr_ill ? 0xfffd : in[i];
+		} else if (!inplace)
 			out[i] = in[i];
 
-		high_surrogate_prev = high_surrogate;
+		prev_hi = curr == 0xd800;
 	}
 
 	/* string may not end with high surrogate */
-	if (high_surrogate_prev)
+	if (prev_hi)
 		out[i - 1] = 0xfffd;
 }
 
+void
+utf16fix_generic(char16_t *out, const char16_t *in, size_t n)
+{
+	if (in == out)
+		utf16fix_inner(out, in, n, true);
+	else
+		utf16fix_inner(out, in, n, false);
+}
