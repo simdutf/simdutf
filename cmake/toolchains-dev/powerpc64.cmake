2262e7a1dc177b949cf62561fb84bf85737cd557
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2025-01-10 16:31:52 +0100

    Add CMake toolchain files for various archs (#643)
    All toolchains were tested with crosscompilers available in Debian.

---
+++ cmake/toolchains-dev/powerpc64.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(target       powerpc64-linux-gnu)
+set(version      14)
+set(c_compiler   gcc)
+set(cxx_compiler g++)
+
+set(CMAKE_C_COMPILER   "${target}-${c_compiler}-${version}")
+set(CMAKE_CXX_COMPILER "${target}-${cxx_compiler}-${version}")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
