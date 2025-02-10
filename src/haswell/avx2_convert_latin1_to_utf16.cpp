57bd27543c0dd71041f01de7fc856541c00c995e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2025-02-10 19:31:59 +0100

    AVX2: better Latin1 <=> UTF-16 conversion

--- src/haswell/avx2_convert_latin1_to_utf16.cpp
+++ src/haswell/avx2_convert_latin1_to_utf16.cpp
@@ -2,35 +2,26 @@ template <endianness big_endian>
 std::pair<const char *, char16_t *>
 avx2_convert_latin1_to_utf16(const char *latin1_input, size_t len,
                              char16_t *utf16_output) {
-  size_t rounded_len = len & ~0xF; // Round down to nearest multiple of 32
+  size_t rounded_len = len & ~0xF; // Round down to nearest multiple of 16
 
   size_t i = 0;
   for (; i < rounded_len; i += 16) {
     // Load 16 bytes from the address (input + i) into a xmm register
-    __m128i xmm0 =
+    const __m128i latin1 =
         _mm_loadu_si128(reinterpret_cast<const __m128i *>(latin1_input + i));
 
-    // Zero extend each byte in xmm0 to word and put it in another xmm register
-    __m128i xmm1 = _mm_cvtepu8_epi16(xmm0);
-
-    // Shift xmm0 to the right by 8 bytes
-    xmm0 = _mm_srli_si128(xmm0, 8);
-
-    // Zero extend each byte in the shifted xmm0 to word in xmm0
-    xmm0 = _mm_cvtepu8_epi16(xmm0);
+    // Zero extend each byte in `in` to word
+    __m256i utf16 = _mm256_cvtepu8_epi16(latin1);
 
     if (big_endian) {
-      const __m128i swap =
+      const __m128i swap128 =
           _mm_setr_epi8(1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14);
-      xmm0 = _mm_shuffle_epi8(xmm0, swap);
-      xmm1 = _mm_shuffle_epi8(xmm1, swap);
+      const __m256i swap = _mm256_set_m128i(swap128, swap128);
+      utf16 = _mm256_shuffle_epi8(utf16, swap);
     }
 
     // Store the contents of xmm1 into the address pointed by (output + i)
-    _mm_storeu_si128(reinterpret_cast<__m128i *>(utf16_output + i), xmm1);
-
-    // Store the contents of xmm0 into the address pointed by (output + i + 8)
-    _mm_storeu_si128(reinterpret_cast<__m128i *>(utf16_output + i + 8), xmm0);
+    _mm256_storeu_si256(reinterpret_cast<__m256i *>(utf16_output + i), utf16);
   }
 
   return std::make_pair(latin1_input + rounded_len, utf16_output + rounded_len);
