51ea9f138eca834f8b29120b78f32b4181b492eb
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2025-02-20 18:05:34 +0100

    Fix wrong implementation

--- src/simdutf/icelake/simd32-inl.h
+++ src/simdutf/icelake/simd32-inl.h
@@ -12,7 +12,13 @@ template <> struct simd32<uint32_t> {
   simdutf_really_inline simd32(const Pointer *ptr)
       : value(_mm512_loadu_si512(reinterpret_cast<const __m512i *>(ptr))) {}
 
-  uint64_t sum() const { return _mm512_reduce_add_epi64(value); }
+  uint64_t sum() const {
+    const __m512i mask = _mm512_set1_epi64(0xffffffff);
+    const __m512i t0 = _mm512_and_si512(value, mask);
+    const __m512i t1 = _mm512_srli_epi64(value, 32);
+    const __m512i t2 = _mm512_add_epi64(t0, t1);
+    return _mm512_reduce_add_epi64(t2);
+  }
 
   // operators
   simdutf_really_inline simd32 &operator+=(const simd32 other) {
