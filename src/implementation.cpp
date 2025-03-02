743b83f5c507d76747be33ba2416decf5d4adf04
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2025-03-02 19:49:59 +0100

    ppc64 target: port most functions to AltiVec
    Port support only big-endian architectures.

--- src/implementation.cpp
+++ src/implementation.cpp
@@ -71,9 +71,16 @@ simdutf_warn_unused encoding_type implementation::autodetect_encoding(
   }
   return encoding_type::unspecified;
 }
+
+  #ifdef SIMDUTF_INTERNAL_TESTS
+std::vector<implementation::TestProcedure>
+implementation::internal_tests() const {
+  return {};
+}
+  #endif
 #endif // SIMDUTF_FEATURE_DETECT_ENCODING
 
-#ifdef SIMDUTF_FEATURE_BASE64
+#if SIMDUTF_FEATURE_BASE64
 simdutf_warn_unused size_t implementation::maximal_binary_length_from_base64(
     const char *input, size_t length) const noexcept {
   return scalar::base64::maximal_binary_length_from_base64(input, length);
