1913a811fe7511a18526d705a2cce7cc25995f93
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2025-04-07 21:31:12 +0200

    haswell: faster UTF-32 to UTF-16
    Add vectorized code for surrogate pair generation.
    This is a port of the westmere solution.

--- src/scalar/swap_bytes.h
+++ src/scalar/swap_bytes.h
@@ -4,11 +4,23 @@
 namespace simdutf {
 namespace scalar {
 
-inline simdutf_warn_unused uint16_t u16_swap_bytes(const uint16_t word) {
+simdutf_really_inline simdutf_warn_unused uint16_t
+u16_swap_bytes(const uint16_t word) {
   return uint16_t((word >> 8) | (word << 8));
 }
 
-inline simdutf_warn_unused uint32_t u32_swap_bytes(const uint32_t word) {
+template <endianness source, endianness destination>
+simdutf_really_inline simdutf_warn_unused uint16_t
+u16_force_endianness(const uint16_t word) {
+  if (source != destination) {
+    return u16_swap_bytes(word);
+  } else {
+    return word;
+  }
+}
+
+simdutf_really_inline simdutf_warn_unused uint32_t
+u32_swap_bytes(const uint32_t word) {
   return ((word >> 24) & 0xff) |      // move byte 3 to byte 0
          ((word << 8) & 0xff0000) |   // move byte 1 to byte 2
          ((word >> 8) & 0xff00) |     // move byte 2 to byte 1
