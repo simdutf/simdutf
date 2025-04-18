555e046a24c8625c221f4b3ef53d97a78c916e6e
Daniel Lemire <daniel@lemire.me>
2025-04-18 14:01:42 -0400

    recovering some of the lost speed in utf-32 to utf-16 (arm) when there is no surrogate handling (#750)
    * recovering some of the lost speed in utf-32 to utf-16 (arm) when there is no surrogates
    
    * adding a likely to the other function too!
    
    * fixing performance regression for Neoverse V1
    
    * lint

--- src/arm64/arm_convert_utf32_to_utf16.cpp
+++ src/arm64/arm_convert_utf32_to_utf16.cpp
@@ -3,7 +3,9 @@ struct expansion_result_t {
   uint8x16_t compressed_v;
 };
 
-static simdutf_really_inline uint64_t invalid_utf32(const uint32x4x2_t in) {
+// This function is used to check for invalid UTF-32 characters
+// and surrogate pairs in the input
+simdutf_really_inline uint64_t invalid_utf32(const uint32x4x2_t in) {
   const auto standardmax = vdupq_n_u32(0x10ffff);
   const auto v_d800 = vdupq_n_u32(0xd800);
   const auto v_fffff800 = vdupq_n_u32(0xfffff800);
@@ -19,8 +21,20 @@ static simdutf_really_inline uint64_t invalid_utf32(const uint32x4x2_t in) {
   return vget_lane_u64(vreinterpret_u64_u8(vshrn_n_u16(err, 8)), 0);
 }
 
+// This function is used to check for surrogate pairs in the input
+simdutf_really_inline uint64_t fast_invalid_utf32(const uint32x4x2_t in) {
+  const auto v_d800 = vdupq_n_u32(0xd800);
+  const auto v_fffff800 = vdupq_n_u32(0xfffff800);
+  const auto surrogate1 = vceqq_u32(vandq_u32(in.val[0], v_fffff800), v_d800);
+  const auto surrogate2 = vceqq_u32(vandq_u32(in.val[1], v_fffff800), v_d800);
+  const auto err = vuzp2q_u16(vreinterpretq_u16_u32(surrogate1),
+                              vreinterpretq_u16_u32(surrogate2));
+  return vget_lane_u64(vreinterpret_u64_u8(vshrn_n_u16(err, 8)), 0);
+}
+
 template <endianness byte_order>
-expansion_result_t neon_expand_surrogate(const uint32x4_t in) {
+simdutf_really_inline expansion_result_t
+neon_expand_surrogate(const uint32x4_t in) {
   const uint32x4_t v_ffff0000 = vdupq_n_u32(0xffff0000);
   const uint32x4_t non_surrogate_mask = vceqzq_u32(vandq_u32(in, v_ffff0000));
   const uint64_t cmp_bits =
@@ -64,7 +78,8 @@ arm_convert_utf32_to_utf16(const char32_t *buf, size_t len,
     uint32x4x2_t in = vld1q_u32_x2(reinterpret_cast<const uint32_t *>(buf));
 
     // Check if no bits set above 16th
-    if (vmaxvq_u32(vorrq_u32(in.val[0], in.val[1])) <= 0xFFFF) {
+    uint32_t max_val = vmaxvq_u32(vmaxq_u32(in.val[0], in.val[1]));
+    if (simdutf_likely(max_val <= 0xFFFF)) {
       uint16x8_t utf16_packed = vuzp1q_u16(vreinterpretq_u16_u32(in.val[0]),
                                            vreinterpretq_u16_u32(in.val[1]));
 
@@ -82,8 +97,7 @@ arm_convert_utf32_to_utf16(const char32_t *buf, size_t len,
       utf16_output += 8;
       buf += 8;
     } else {
-      const uint64_t err = invalid_utf32(in);
-      if (simdutf_unlikely(err)) {
+      if (simdutf_unlikely(fast_invalid_utf32(in) || max_val > 0x10ffff)) {
         return std::make_pair(nullptr,
                               reinterpret_cast<char16_t *>(utf16_output));
       }
@@ -98,7 +112,7 @@ arm_convert_utf32_to_utf16(const char32_t *buf, size_t len,
   }
 
   // check for invalid input
-  if (vmaxvq_u16(forbidden_bytemask) != 0) {
+  if (vmaxvq_u32(vreinterpretq_u32_u16(forbidden_bytemask)) != 0) {
     return std::make_pair(nullptr, reinterpret_cast<char16_t *>(utf16_output));
   }
 
@@ -119,7 +133,8 @@ arm_convert_utf32_to_utf16_with_errors(const char32_t *buf, size_t len,
     uint32x4x2_t in = vld1q_u32_x2(reinterpret_cast<const uint32_t *>(buf));
 
     // Check if no bits set above 16th
-    if (vmaxvq_u32(vorrq_u32(in.val[0], in.val[1])) <= 0xFFFF) {
+    uint32_t max_val = vmaxvq_u32(vmaxq_u32(in.val[0], in.val[1]));
+    if (simdutf_likely(max_val <= 0xFFFF)) {
       uint16x8_t utf16_packed = vuzp1q_u16(vreinterpretq_u16_u32(in.val[0]),
                                            vreinterpretq_u16_u32(in.val[1]));
 
@@ -140,7 +155,8 @@ arm_convert_utf32_to_utf16_with_errors(const char32_t *buf, size_t len,
       utf16_output += 8;
       buf += 8;
     } else {
-      const uint64_t err = invalid_utf32(in);
+      const uint64_t err =
+          max_val <= 0x10ffff ? fast_invalid_utf32(in) : invalid_utf32(in);
       if (simdutf_unlikely(err)) {
         const size_t pos = trailing_zeroes(err) / 8;
         for (size_t k = 0; k < pos; k++) {
