03e1f9f3e61448fd047621cd8b6f50c219456133
Daniel Lemire <daniel@lemire.me>
2025-05-25 20:12:22 -0400

    release candidate 7.2.0 (#800)
    * release candidate 7.2.0
    
    * lint

--- src/implementation.cpp
+++ src/implementation.cpp
@@ -2084,14 +2084,13 @@ simdutf_warn_unused size_t utf32_length_from_utf8(const char *input,
 
 #if SIMDUTF_FEATURE_BASE64
 simdutf_warn_unused const char *find(const char *start, const char *end,
-                          char character) noexcept {
-  return get_default_implementation()->find(start, end, character); 
-                          }
-simdutf_warn_unused const char16_t *find(const char16_t *start, const char16_t *end,
-                              char16_t character) noexcept {
+                                     char character) noexcept {
   return get_default_implementation()->find(start, end, character);
-                              }
-
+}
+simdutf_warn_unused const char16_t *
+find(const char16_t *start, const char16_t *end, char16_t character) noexcept {
+  return get_default_implementation()->find(start, end, character);
+}
 
 simdutf_warn_unused size_t
 maximal_binary_length_from_base64(const char *input, size_t length) noexcept {
