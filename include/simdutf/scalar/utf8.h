6bf882d7db829c961afab4b80db1e3b46bfe9e12
Paul Dreik <github@pauldreik.se>
2025-12-06 09:36:55 +0100

    fix claimed UB in utf8::validate
    it was not used uninitialized, it is memcpy:ed over on the next line.

--- include/simdutf/scalar/utf8.h
+++ include/simdutf/scalar/utf8.h
@@ -17,9 +17,9 @@ inline simdutf_warn_unused bool validate(const char *buf, size_t len) noexcept {
     uint64_t next_pos = pos + 16;
     if (next_pos <=
         len) { // if it is safe to read 16 more bytes, check that they are ascii
-      uint64_t v1;
+      uint64_t v1{};
       std::memcpy(&v1, data + pos, sizeof(uint64_t));
-      uint64_t v2;
+      uint64_t v2{};
       std::memcpy(&v2, data + pos + sizeof(uint64_t), sizeof(uint64_t));
       uint64_t v{v1 | v2};
       if ((v & 0x8080808080808080) == 0) {
