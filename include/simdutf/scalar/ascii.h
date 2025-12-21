33e45fc0c342e0100904b8ba7a50725ab3862c19
Paul Dreik <github@pauldreik.se>
2025-12-21 20:11:35 +0100

    make validate_ascii_with_errors() constexpr

--- include/simdutf/scalar/ascii.h
+++ include/simdutf/scalar/ascii.h
@@ -40,29 +40,38 @@ simdutf_warn_unused simdutf_constexpr23 bool validate(InputPtr data,
   }
   return true;
 }
-
-inline simdutf_warn_unused result validate_with_errors(const char *buf,
-                                                       size_t len) noexcept {
-  const uint8_t *data = reinterpret_cast<const uint8_t *>(buf);
+template <class InputPtr>
+#if SIMDUTF_CPLUSPLUS20
+  requires simdutf::detail::indexes_into_byte_like<InputPtr>
+#endif
+simdutf_warn_unused simdutf_constexpr23 result
+validate_with_errors(InputPtr data, size_t len) noexcept {
   size_t pos = 0;
-  // process in blocks of 16 bytes when possible
-  for (; pos + 16 <= len; pos += 16) {
-    uint64_t v1;
-    std::memcpy(&v1, data + pos, sizeof(uint64_t));
-    uint64_t v2;
-    std::memcpy(&v2, data + pos + sizeof(uint64_t), sizeof(uint64_t));
-    uint64_t v{v1 | v2};
-    if ((v & 0x8080808080808080) != 0) {
-      for (; pos < len; pos++) {
-        if (data[pos] >= 0b10000000) {
-          return result(error_code::TOO_LARGE, pos);
+#if SIMDUTF_CPLUSPLUS23
+  // avoid memcpy during constant evaluation
+  if !consteval
+#endif
+  {
+    // process in blocks of 16 bytes when possible
+    for (; pos + 16 <= len; pos += 16) {
+      uint64_t v1;
+      std::memcpy(&v1, data + pos, sizeof(uint64_t));
+      uint64_t v2;
+      std::memcpy(&v2, data + pos + sizeof(uint64_t), sizeof(uint64_t));
+      uint64_t v{v1 | v2};
+      if ((v & 0x8080808080808080) != 0) {
+        for (; pos < len; pos++) {
+          if (static_cast<std::uint8_t>(data[pos]) >= 0b10000000) {
+            return result(error_code::TOO_LARGE, pos);
+          }
         }
       }
     }
   }
+
   // process the tail byte-by-byte
   for (; pos < len; pos++) {
-    if (data[pos] >= 0b10000000) {
+    if (static_cast<std::uint8_t>(data[pos]) >= 0b10000000) {
       return result(error_code::TOO_LARGE, pos);
     }
   }
--- tests/validate_ascii_with_errors_tests.cpp
+++ tests/validate_ascii_with_errors_tests.cpp
@@ -1,5 +1,6 @@
 #include "simdutf.h"
 
+#include <tests/helpers/fixed_string.h>
 #include <tests/helpers/random_utf8.h>
 #include <tests/helpers/test.h>
 
@@ -31,5 +32,46 @@ TEST_LOOP(error_ASCII) {
     ascii[i] -= 0b10000000;
   }
 }
+
+#if SIMDUTF_CPLUSPLUS23
+
+namespace {
+// for negative compilation tests
+template <class InputPtr>
+concept passable_to_validate = requires(InputPtr p) {
+  simdutf::scalar::ascii::validate_with_errors(p, 10u);
+};
+
+} // namespace
+
+TEST(compile_time_valid) {
+
+  using namespace simdutf::tests::helpers;
+  constexpr auto ascii = "a normal ascii text"_latin1;
+
+  static_assert(simdutf::validate_ascii_with_errors(ascii).is_ok());
+  static_assert(
+      simdutf::validate_ascii_with_errors(ascii.as_array<unsigned char>())
+          .is_ok());
+  static_assert(
+      simdutf::validate_ascii_with_errors(ascii.as_array<signed char>())
+          .is_ok());
+  static_assert(
+      simdutf::validate_ascii_with_errors(ascii.as_array<std::byte>()).is_ok());
+
+  static_assert(passable_to_validate<char *>);
+  static_assert(passable_to_validate<unsigned char *>);
+  static_assert(passable_to_validate<const char *>);
+  static_assert(!passable_to_validate<int *>);
+  static_assert(passable_to_validate<std::array<char, 10>>);
+  static_assert(!passable_to_validate<std::array<int, 10>>);
+}
+
+TEST(compile_time_invalid) {
+  using namespace simdutf::tests::helpers;
+  constexpr auto not_ascii = u8"not ascii: köttbulle"_utf8;
+  static_assert(simdutf::validate_ascii_with_errors(not_ascii).is_err());
+}
+#endif
 
 TEST_MAIN
