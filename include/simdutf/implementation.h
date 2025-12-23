37189f743f6adc8c2466698ee8cb438a8a1add59
Paul Dreik <github@pauldreik.se>
2025-12-23 22:50:40 +0100

    make convert_latin1_to_utf8_safe() constexpr
    duplicate parts of the implementation to avoid working around the
    memcpy block which is forbidden in constexpr evaluation.

--- include/simdutf/implementation.h
+++ include/simdutf/implementation.h
@@ -833,18 +833,27 @@ simdutf_warn_unused size_t
 convert_latin1_to_utf8_safe(const char *input, size_t length, char *utf8_output,
                             size_t utf8_len) noexcept;
   #if SIMDUTF_SPAN
-simdutf_really_inline simdutf_warn_unused size_t convert_latin1_to_utf8_safe(
+simdutf_really_inline simdutf_warn_unused simdutf_constexpr23 size_t
+convert_latin1_to_utf8_safe(
     const detail::input_span_of_byte_like auto &input,
     detail::output_span_of_byte_like auto &&utf8_output) noexcept {
-  // implementation note: outputspan is a forwarding ref to avoid copying and
-  // allow both lvalues and rvalues. std::span can be copied without problems,
-  // but std::vector should not, and this function should accept both. it will
-  // allow using an owning rvalue ref (example: passing a temporary std::string)
-  // as output, but the user will quickly find out that he has no way of getting
-  // the data out of the object in that case.
-  return convert_latin1_to_utf8_safe(
-      input.data(), input.size(), reinterpret_cast<char *>(utf8_output.data()),
-      utf8_output.size());
+      // implementation note: outputspan is a forwarding ref to avoid copying
+      // and allow both lvalues and rvalues. std::span can be copied without
+      // problems, but std::vector should not, and this function should accept
+      // both. it will allow using an owning rvalue ref (example: passing a
+      // temporary std::string) as output, but the user will quickly find out
+      // that he has no way of getting the data out of the object in that case.
+    #if SIMDUTF_CPLUSPLUS23
+  if consteval {
+    return scalar::latin1_to_utf8::convert_safe_constexpr(
+        input.data(), input.size(), utf8_output.data(), utf8_output.size());
+  } else
+    #endif
+  {
+    return convert_latin1_to_utf8_safe(
+        input.data(), input.size(),
+        reinterpret_cast<char *>(utf8_output.data()), utf8_output.size());
+  }
 }
   #endif // SIMDUTF_SPAN
 #endif   // SIMDUTF_FEATURE_UTF8 && SIMDUTF_FEATURE_LATIN1
