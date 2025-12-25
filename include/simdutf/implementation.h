68d2be4064f059b910f6da259356b8abb5d3ae77
Paul Dreik <github@pauldreik.se>
2025-12-25 07:53:05 +0100

    fix feature macro mistakes

--- include/simdutf/implementation.h
+++ include/simdutf/implementation.h
@@ -2966,10 +2966,15 @@ trim_partial_utf16(std::span<const char16_t> valid_utf16_input) noexcept {
   #endif // SIMDUTF_SPAN
 #endif   // SIMDUTF_FEATURE_UTF16
 
-#if SIMDUTF_FEATURE_BASE64
+#if SIMDUTF_FEATURE_BASE64 || SIMDUTF_FEATURE_UTF16 ||                         \
+    SIMDUTF_FEATURE_DETECT_ENCODING
   #ifndef SIMDUTF_NEED_TRAILING_ZEROES
     #define SIMDUTF_NEED_TRAILING_ZEROES 1
   #endif
+#endif // SIMDUTF_FEATURE_BASE64 || SIMDUTF_FEATURE_UTF16 ||
+       // SIMDUTF_FEATURE_DETECT_ENCODING
+
+#if SIMDUTF_FEATURE_BASE64
 // base64_options are used to specify the base64 encoding options.
 // ASCII spaces are ' ', '\t', '\n', '\r', '\f'
 // garbage characters are characters that are not part of the base64 alphabet
