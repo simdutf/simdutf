f522270d0df3829bfe22b58bce10e81bf3c3f4e5
Daniel Lemire <daniel@lemire.me>
2026-01-31 13:29:47 -0500

    for short inputs get_remainder is inefficient.

--- src/generic/ascii_validation.h
+++ src/generic/ascii_validation.h
@@ -4,44 +4,41 @@ namespace {
 namespace ascii_validation {
 
 result generic_validate_ascii_with_errors(const char *input, size_t length) {
-  buf_block_reader<64> reader(reinterpret_cast<const uint8_t *>(input), length);
-  size_t count{0};
-  while (reader.has_full_block()) {
-    simd::simd8x64<uint8_t> in(reader.full_block());
-    if (!in.is_ascii()) {
-      result res = scalar::ascii::validate_with_errors(
-          reinterpret_cast<const char *>(input + count), length - count);
-      return result(res.error, count + res.count);
-    }
-    reader.advance();
+  if(length >= 64) {
+    buf_block_reader<64> reader(reinterpret_cast<const uint8_t *>(input), length);
+    size_t count{0};
+    while (reader.has_full_block()) {
+      simd::simd8x64<uint8_t> in(reader.full_block());
+      if (!in.is_ascii()) {
+        result res = scalar::ascii::validate_with_errors(
+            reinterpret_cast<const char *>(input + count), length - count);
+        return result(res.error, count + res.count);
+      }
+      reader.advance();
 
-    count += 64;
+      count += 64;
+    }
   }
-  uint8_t block[64]{};
-  reader.get_remainder(block);
-  simd::simd8x64<uint8_t> in(block);
-  if (!in.is_ascii()) {
-    result res = scalar::ascii::validate_with_errors(
-        reinterpret_cast<const char *>(input + count), length - count);
-    return result(res.error, count + res.count);
-  } else {
-    return result(error_code::SUCCESS, length);
-  }
+  result res = scalar::ascii::validate_with_errors(
+      reinterpret_cast<const char *>(input + count), length - count);
+  return result(res.error, count + res.count);
 }
 
 bool generic_validate_ascii(const char *input, size_t length) {
-  buf_block_reader<64> reader(reinterpret_cast<const uint8_t *>(input), length);
-  while (reader.has_full_block()) {
-    simd::simd8x64<uint8_t> in(reader.full_block());
-    if (!in.is_ascii()) {
-      return false;
+  if(length >= 64) {
+    buf_block_reader<64> reader(reinterpret_cast<const uint8_t *>(input), length);
+    while (reader.has_full_block()) {
+      simd::simd8x64<uint8_t> in(reader.full_block());
+      if (!in.is_ascii()) {
+        return false;
+      }
+      reader.advance();
     }
-    reader.advance();
   }
-  uint8_t block[64]{};
-  reader.get_remainder(block);
-  simd::simd8x64<uint8_t> in(block);
-  return in.is_ascii();
+  // We complete with 'naive' code.
+  // We use optimistic code to help the compiler.
+  return scalar::ascii::validate(
+      reinterpret_cast<const char *>(input + (length / 64) * 64), length % 64);
 }
 
 } // namespace ascii_validation
--- src/generic/buf_block_reader.h
+++ src/generic/buf_block_reader.h
@@ -10,17 +10,6 @@ public:
   simdutf_really_inline size_t block_index();
   simdutf_really_inline bool has_full_block() const;
   simdutf_really_inline const uint8_t *full_block() const;
-  /**
-   * Get the last block, padded with spaces.
-   *
-   * There will always be a last block, with at least 1 byte, unless len == 0
-   * (in which case this function fills the buffer with spaces and returns 0. In
-   * particular, if len == STEP_SIZE there will be 0 full_blocks and 1 remainder
-   * block with STEP_SIZE bytes and no spaces for padding.
-   *
-   * @return the number of effective characters in the last block.
-   */
-  simdutf_really_inline size_t get_remainder(uint8_t *dst) const;
   simdutf_really_inline void advance();
 
 private:
@@ -84,19 +73,6 @@ template <size_t STEP_SIZE>
 simdutf_really_inline const uint8_t *
 buf_block_reader<STEP_SIZE>::full_block() const {
   return &buf[idx];
-}
-
-template <size_t STEP_SIZE>
-simdutf_really_inline size_t
-buf_block_reader<STEP_SIZE>::get_remainder(uint8_t *dst) const {
-  if (len == idx) {
-    return 0;
-  } // memcpy(dst, null, 0) will trigger an error with some sanitizers
-  std::memset(dst, 0x20,
-              STEP_SIZE); // std::memset STEP_SIZE because it is more efficient
-                          // to write out 8 or 16 bytes at once.
-  std::memcpy(dst, buf + idx, len - idx);
-  return len - idx;
 }
 
 template <size_t STEP_SIZE>
