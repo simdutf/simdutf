ac7c4e4bd7f8b886341bd0164c541bee6ba39127
Daniel Lemire <daniel@lemire.me>
2026-04-09 15:17:59 -0400

    reduce the size of our tools

--- include/simdutf/implementation.h
+++ include/simdutf/implementation.h
@@ -31,13 +31,27 @@
 //      #   error("Please amalgamate simdutf with UTF-16 support")
 //      #endif
 //
+#ifndef SIMDUTF_FEATURE_DETECT_ENCODING
 #define SIMDUTF_FEATURE_DETECT_ENCODING 1
+#endif
+#ifndef SIMDUTF_FEATURE_ASCII
 #define SIMDUTF_FEATURE_ASCII 1
+#endif
+#ifndef SIMDUTF_FEATURE_LATIN1
 #define SIMDUTF_FEATURE_LATIN1 1
+#endif
+#ifndef SIMDUTF_FEATURE_UTF8
 #define SIMDUTF_FEATURE_UTF8 1
+#endif
+#ifndef SIMDUTF_FEATURE_UTF16
 #define SIMDUTF_FEATURE_UTF16 1
+#endif
+#ifndef SIMDUTF_FEATURE_UTF32
 #define SIMDUTF_FEATURE_UTF32 1
+#endif
+#ifndef SIMDUTF_FEATURE_BASE64
 #define SIMDUTF_FEATURE_BASE64 1
+#endif
 
 #if SIMDUTF_CPLUSPLUS23
   #include <simdutf/constexpr_ptr.h>
