e901b35767b2cf1a8e7b9af74da87765d6e093b5
Benjamin Bucher <>
2026-05-20 17:46:58 +0200

    wip generic implementation

--- src/generic/utf8_validation/utf8_validator.h
+++ src/generic/utf8_validation/utf8_validator.h
@@ -78,6 +78,67 @@ result generic_validate_utf8_with_errors(const char *input, size_t length) {
       reinterpret_cast<const uint8_t *>(input), length);
 }
 
+/**
+ * Validates that the string is actual UTF-8 and stops on errors.
+ * Tracks the amount of continuation and 4-byte leads.
+ */
+template <class checker>
+utf8_result generic_validate_utf8_with_errors(const uint8_t *input,
+                                              size_t length) {
+  checker c{};
+  buf_block_reader<64> reader(input, length);
+  size_t count{0};
+  while (reader.has_full_block()) {
+    simd::simd8x64<uint8_t> in(reader.full_block());
+    c.check_next_input(in);
+    if (c.errors()) {
+      /*
+       * Why was this done?
+      if (count != 0) {
+        count--;
+      } // Sometimes the error is only detected in the next chunk
+      */
+      result res = scalar::utf8::rewind_and_validate_with_counts(
+          reinterpret_cast<const char *>(input),
+          reinterpret_cast<const char *>(input + count), length - count);
+      res.input_count += count;
+      res.continuations += c.continuation_count();
+      res.four_byte += c.four_byte_count();
+      return res;
+    }
+    reader.advance();
+    count += 64;
+  }
+  uint8_t block[64]{};
+  reader.get_remainder(block);
+  simd::simd8x64<uint8_t> in(block);
+  c.check_next_input(in);
+  reader.advance();
+  c.check_eof();
+  if (c.errors()) {
+    /*
+     * Why was this done?
+  if (count != 0) {
+    count--;
+  } // Sometimes the error is only detected in the next chunk
+  */
+    result res = scalar::utf8::rewind_and_validate_with_counts(
+        reinterpret_cast<const char *>(input),
+        reinterpret_cast<const char *>(input) + count, length - count);
+    res.count += count;
+    res.continuations += c.continuation_count();
+    res.four_byte += c.continuation_count();
+    return res;
+  } else {
+    return result(error_code::SUCCESS, length, c.continuation_count(),
+                  c.four_byte_count());
+  }
+}
+
+result generic_validate_utf8_with_errors(const char *input, size_t length) {
+  return generic_validate_utf8_with_errors<utf8_checker>(
+      reinterpret_cast<const uint8_t *>(input), length);
+
 } // namespace utf8_validation
 } // unnamed namespace
 } // namespace SIMDUTF_IMPLEMENTATION
