3fb9ad0346a33e8187747d2aac528cfffa50afc7
Benjamin Bucher <>
2026-05-20 15:50:09 +0200

    Revert "use scalar implementation everywhere for now"
    This reverts commit 341d60eb93fe5700949f96e4a3d55b9cea73c4e2.

--- src/lasx/implementation.cpp
+++ src/lasx/implementation.cpp
@@ -1335,7 +1335,7 @@ implementation::convert_utf16be_to_utf8_with_replacement(
 simdutf_warn_unused full_result
 implementation::validate_utf8_for_utf16_transcoding(const char *buf,
                                                     size_t len) const noexcept {
-  return scalar::utf8_to_utf16::validate_utf8_for_utf16_transcoding(buf, len);
+  // TODO: implement
 }
 
 #endif // SIMDUTF_FEATURE_UTF8 && SIMDUTF_FEATURE_UTF16
