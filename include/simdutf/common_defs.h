21b17bac0c565ca2b5e2a8bdbd7e0da089dd449e
Daniel Lemire <daniel@lemire.me>
2026-06-12 19:35:18 -0400

    upgrading our 'unused' macros + silence silly warnings on 3 functions

--- include/simdutf/common_defs.h
+++ include/simdutf/common_defs.h
@@ -24,6 +24,20 @@
   #define simdutf_log_assert(cond, msg)
 #endif
 
+#if SIMDUTF_CPLUSPLUS17
+#define simdutf_unused [[maybe_unused]]
+#endif // SIMDUTF_CPLUSPLUS17
+
+#if SIMDUTF_CPLUSPLUS20
+#if defined(__has_cpp_attribute)
+  #if __has_cpp_attribute(likely) >= 201803L && __has_cpp_attribute(unlikely) >= 201803L
+    #define simdutf_likely [[likely]]
+    #define simdutf_unlikely [[unlikely]]
+  #endif // __has_cpp_attribute(likely)
+#endif // __has_cpp_attribute
+#endif // SIMDUTF_CPLUSPLUS20
+
+
 #if defined(SIMDUTF_REGULAR_VISUAL_STUDIO)
   #define SIMDUTF_DEPRECATED __declspec(deprecated)
 
@@ -31,7 +45,9 @@
   #define simdutf_always_inline __forceinline // always inline, no matter what
   #define simdutf_never_inline __declspec(noinline)
 
+  #ifndef simdutf_unused
   #define simdutf_unused
+  #endif // simdutf_unused
   #define simdutf_warn_unused
 
   #ifndef simdutf_likely
@@ -74,8 +90,9 @@
     inline __attribute__((always_inline)) // always inline, no matter what
   #define SIMDUTF_DEPRECATED __attribute__((deprecated))
   #define simdutf_never_inline inline __attribute__((noinline))
-
+  #ifndef simdutf_unused
   #define simdutf_unused __attribute__((unused))
+  #endif // simdutf_unused
   #define simdutf_warn_unused __attribute__((warn_unused_result))
 
   #ifndef simdutf_likely
