815d9236589a1a253d3d54436a9100a839daa1d2
Daniel Lemire <daniel@lemire.me>
2026-09-25 16:42:39 -0400

    typos: ignore 'caf' (false positive in test strings)

--- scripts/check_typos.sh
+++ scripts/check_typos.sh
@@ -8,6 +8,6 @@ set -eu
 # this exits with nonzero status if it finds something, which terminates the script
 codespell \
     --skip="./benchmarks/competition,./build,./fuzz/work"  \
-    -L vie,persan,fo,ans,larg,indx,shft,carryin,statics
+    -L vie,persan,fo,ans,larg,indx,shft,carryin,statics,caf
 
 echo "no typos detected!"
